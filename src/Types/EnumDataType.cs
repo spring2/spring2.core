044209ab2c72c90690734aa3783520cab5da84e7
cort <none@none>
2002-04-13 01:18:59 +0000

    add base class place holder for generated enum data types

---
+++ src/Types/EnumDataType.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Spring2.Core.Types
+{
+	/// <summary>
+	/// Summary description for EnumDataType.
+	/// </summary>
+	public class EnumDataType
+	{
+		public EnumDataType()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+
+        // implement to string that uses internal value
+	}
+}
