1f5427808e4fa1d9debe8803f463fca7d23648f7
cort <none@none>
2002-04-25 14:27:57 +0000

    support anding multiple field/value pairs together

--- src/DAO/WhereClause.cs
+++ src/DAO/WhereClause.cs
@@ -3,7 +3,10 @@ using System;
 namespace Spring2.Core.DAO {
     public class WhereClause : IWhere {
 
-	private String sql;
+	private String sql = String.Empty;
+
+	public WhereClause() {
+	}
 
 	public WhereClause(String field, Int32 value) {
 	    sql = field + "=" + value.ToString();
@@ -22,7 +25,43 @@ namespace Spring2.Core.DAO {
 	}
 
 	public String FormatSql() {
-	    return " where " + sql;
+	    if (!sql.Equals(String.Empty)) {
+		return " where " + sql;
+	    } else {
+		return String.Empty;
+	    }
 	}
+
+	public void And(String field, Int32 value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " and ";
+	    }
+	    sql += field + "=" + value.ToString();
+	}
+
+	public void And(String field, String value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " and ";
+	    }
+	    sql += field + "='" + value + "'";
+	}
+
+	public void And(String field, DateTime value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " and ";
+	    }
+	    sql += field + "='" + value.ToString() + "'";
+	}
+
+	public void And(String field, Object value) {
+	    if (value is Int32) {
+		And(field, (Int32)value);
+	    } else if (value is DateTime) {
+		And(field, (DateTime)value);
+	    } else {
+		And(field, value.ToString());
+	    }
+	}
+
     }
 }
