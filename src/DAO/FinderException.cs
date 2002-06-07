989049a03630c92644795a89e45a675a53d35340
bspiglejr <none@none>
2002-06-07 17:23:35 +0000

    Finder exception thrown when no rows found in list or load methods.

---
+++ src/DAO/FinderException.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Spring2.Core.DAO
+{
+    /// <summary>
+    /// Exception created when a finder returns no rows.
+    /// </summary>
+    public class FinderException : System.ApplicationException
+    {
+	public FinderException() {}
+
+	public FinderException (String message) : base(message) {}
+    }
+}
