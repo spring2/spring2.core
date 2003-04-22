39ac4a73d44d7d0aeb9f1688f8ab1af6e74007b4
cort <none@none>
2003-04-22 22:12:38 +0000

    add AndEquals, OrEquals, AndNotEquals, OrNotEquals, AndBetween, AndNotBetween, OrBetween, OrNotBetween methods to WhereClause

--- src/DAO/WhereClause.cs
+++ src/DAO/WhereClause.cs
@@ -1,118 +1,445 @@
 using System;
 
 namespace Spring2.Core.DAO {
+
+    /// <summary>
+    /// Utility class to construct WHERE clause sql fragments
+    /// </summary>
     public class WhereClause : IWhere {
 
 	private String sql = String.Empty;
 
+	#region Constructors
+	/// <summary>
+	/// New empty clause
+	/// </summary>
 	public WhereClause() {
 	}
 
+	/// <summary>
+	/// New clause that will check that column equals value
+	/// </summary>
+	/// <remarks>
+	/// field = value
+	/// </remarks>
 	public WhereClause(String field, Int32 value) {
 	    sql = field + "=" + value.ToString();
 	}
 
+	/// <summary>
+	/// New clause that will check that column equals value
+	/// </summary>
+	/// <remarks>
+	/// field = value
+	/// </remarks>
 	public WhereClause(String field, String value) {
 	    sql = field + "='" + value.Replace("'", "''") + "'";
 	}
 
+	/// <summary>
+	/// New clause that will check that column equals value
+	/// </summary>
+	/// <remarks>
+	/// field = value
+	/// </remarks>
 	public WhereClause(String field, DateTime value) {
 	    sql = field + "='" + value.ToString() + "'";
 	}
 
+	/// <summary>
+	/// New clause using raw sql
+	/// </summary>
 	public WhereClause(String sql) {
 	    this.sql = sql;
 	}
 
+	/// <summary>
+	/// New clause using WhereClause (for wrapping with enclosing parens)
+	/// </summary>
+	/// <remarks>
+	/// ( clause )
+	/// </remarks>
 	public WhereClause(WhereClause wc) {
 	    // Note that this is using the internal sql string so that additional formatting is not added
 	    this.sql = "(" + wc.ToString() + ") ";
+    	}
+	#endregion
+
+	#region And
+	public void And(WhereClause wc) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += " (" + wc.ToString() + ") ";
 	}
 
+	public void And(String field, Int32 value) {
+	    AndEquals(field, value);
+	}
+
+	public void And(String field, String value) {
+	    AndEquals(field, value);
+	}
+
+	public void And(String field, DateTime value) {
+	    AndEquals(field, value);
+	}
+
+	public void And(String field, Object value) {
+	    AndEquals(field, value);
+	}
+	#endregion
+
+	#region AndEquals
+	public void AndEquals(String field, Int32 value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + "=" + value.ToString();
+	}
+
+	public void AndEquals(String field, String value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + "='" + value.Replace("'", "''") + "'";
+	}
+
+	public void AndEquals(String field, DateTime value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + "='" + value.ToString() + "'";
+	}
+
+	public void AndEquals(String field, Object value) {
+	    if (value is Int32) {
+		AndEquals(field, (Int32)value);
+	    } else if (value is DateTime) {
+		AndEquals(field, (DateTime)value);
+	    } else {
+		AndEquals(field, value.ToString().Replace("'", "''"));
+	    }
+	}
+	#endregion
+
+	#region AndNotEquals
+	public void AndNotEquals(String field, Int32 value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + "<>" + value.ToString();
+	}
+
+	public void AndNotEquals(String field, String value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + "<>'" + value.Replace("'", "''") + "'";
+	}
+
+	public void AndNotEquals(String field, DateTime value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + "<>'" + value.ToString() + "'";
+	}
+
+	public void AndNotEquals(String field, Object value) {
+	    if (value is Int32) {
+		AndNotEquals(field, (Int32)value);
+	    } else if (value is DateTime) {
+		AndNotEquals(field, (DateTime)value);
+	    } else {
+		AndNotEquals(field, value.ToString().Replace("'", "''"));
+	    }
+	}
+	#endregion
+
+	#region Or
+	public void Or(WhereClause wc) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += " (" + wc.ToString() + ") ";
+	}
+
+	public void Or(String field, Int32 value) {
+	    OrEquals(field, value);
+	}
+
+	public void Or(String field, String value) {
+	    OrEquals(field, value);
+	}
+
+	public void Or(String field, DateTime value) {
+	    OrEquals(field, value);
+	}
+
+	public void Or(String field, Object value) {
+	    OrEquals(field, value);
+	}
+	#endregion
+
+	#region OrEquals
+	public void OrEquals(String field, Int32 value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + "=" + value.ToString();
+	}
+
+	public void OrEquals(String field, String value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + "='" + value.Replace("'", "''") + "'";
+	}
+
+	public void OrEquals(String field, DateTime value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + "='" + value.ToString() + "'";
+	}
+
+	public void OrEquals(String field, Object value) {
+	    if (value is Int32) {
+		OrEquals(field, (Int32)value);
+	    } else if (value is DateTime) {
+		OrEquals(field, (DateTime)value);
+	    } else {
+		OrEquals(field, value.ToString().Replace("'", "''"));
+	    }
+	}
+	#endregion
+
+	#region OrNotEquals
+	public void OrNotEquals(String field, Int32 value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + "<>" + value.ToString();
+	}
+
+	public void OrNotEquals(String field, String value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + "<>'" + value.Replace("'", "''") + "'";
+	}
+
+	public void OrNotEquals(String field, DateTime value) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + "<>'" + value.ToString() + "'";
+	}
+
+	public void OrNotEquals(String field, Object value) {
+	    if (value is Int32) {
+		OrNotEquals(field, (Int32)value);
+	    } else if (value is DateTime) {
+		OrNotEquals(field, (DateTime)value);
+	    } else {
+		OrNotEquals(field, value.ToString().Replace("'", "''"));
+	    }
+	}
+	#endregion
+
+	#region AndBetween
+	/// <summary>
+	/// And new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// AND field between value1 AND value2
+	/// </remarks>
+	public void AndBetween(String field, Int32 value1, Int32 value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + " between " + value1.ToString() + " and " + value2.ToString();
+	}
+
+	/// <summary>
+	/// And new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// AND field between value1 AND value2
+	/// </remarks>
+	public void AndBetween(String field, String value1, String value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + " between '" + value1.Replace("'", "''") + "' and '" + value2.Replace("'", "''") + "'";
+	}
+
+	/// <summary>
+	/// And new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// AND field between value1 AND value2
+	/// </remarks>
+	public void AndBetween(String field, DateTime value1, DateTime value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + " between '" + value1.ToString() + "' and '" + value2.ToString() + "'";
+	}
+	#endregion
+
+	#region OrBetween
+	/// <summary>
+	/// Or new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// OR field between value1 AND value2
+	/// </remarks>
+	public void OrBetween(String field, Int32 value1, Int32 value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + " between " + value1.ToString() + " and " + value2.ToString();
+	}
+
+	/// <summary>
+	/// Or new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// OR field between value1 AND value2
+	/// </remarks>
+	public void OrBetween(String field, String value1, String value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + " between '" + value1.Replace("'", "''") + "' and '" + value2.Replace("'", "''") + "'";
+	}
+
+	/// <summary>
+	/// Or new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// OR field between value1 AND value2
+	/// </remarks>
+	public void OrBetween(String field, DateTime value1, DateTime value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + " between '" + value1.ToString() + "' and '" + value2.ToString() + "'";
+	}
+	#endregion
+
+	#region AndNotBetween
+	/// <summary>
+	/// And new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// AND field between value1 AND value2
+	/// </remarks>
+	public void AndNotBetween(String field, Int32 value1, Int32 value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + " NOT between " + value1.ToString() + " and " + value2.ToString();
+	}
+
+	/// <summary>
+	/// And new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// AND field between value1 AND value2
+	/// </remarks>
+	public void AndNotBetween(String field, String value1, String value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + " NOT between '" + value1.Replace("'", "''") + "' and '" + value2.Replace("'", "''") + "'";
+	}
+
+	/// <summary>
+	/// And new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// AND field between value1 AND value2
+	/// </remarks>
+	public void AndNotBetween(String field, DateTime value1, DateTime value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " AND ";
+	    }
+	    sql += field + " NOT between '" + value1.ToString() + "' and '" + value2.ToString() + "'";
+	}
+	#endregion
+
+	#region OrNotBetween
+	/// <summary>
+	/// Or new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// OR field between value1 AND value2
+	/// </remarks>
+	public void OrNotBetween(String field, Int32 value1, Int32 value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + " NOT between " + value1.ToString() + " and " + value2.ToString();
+	}
+
+	/// <summary>
+	/// Or new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// OR field between value1 AND value2
+	/// </remarks>
+	public void OrNotBetween(String field, String value1, String value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + " NOT between '" + value1.Replace("'", "''") + "' and '" + value2.Replace("'", "''") + "'";
+	}
+
+	/// <summary>
+	/// Or new clause that will check that column is between value1 and value2
+	/// </summary>
+	/// <remarks>
+	/// OR field between value1 AND value2
+	/// </remarks>
+	public void OrNotBetween(String field, DateTime value1, DateTime value2) {
+	    if (!sql.Equals(String.Empty)) {
+		sql += " OR ";
+	    }
+	    sql += field + " NOT between '" + value1.ToString() + "' and '" + value2.ToString() + "'";
+	}
+	#endregion
+
+	#region AndIn
+	#endregion
+
+	#region OrIn
+	#endregion
+
+	#region AndNotIn
+	#endregion
+
+	#region OrNotIn
+	#endregion
+
+	/// <summary>
+	/// Format the clause for inclusion in sql statement    
+	/// </summary>
+	/// <remarks>
+	/// WHERE &lt;sql&gt;
+	/// </remarks>
 	public String FormatSql() {
 	    if (!sql.Equals(String.Empty)) {
-		return " where " + sql;
+		return " WHERE " + sql;
 	    } else {
 		return String.Empty;
 	    }
 	}
 
-	public void And(String field, Int32 value) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " and ";
-	    }
-	    sql += field + "=" + value.ToString();
-	}
-
-	public void And(String field, String value) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " and ";
-	    }
-	    sql += field + "='" + value.Replace("'", "''") + "'";
-	}
-
-	public void And(String field, DateTime value) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " and ";
-	    }
-	    sql += field + "='" + value.ToString() + "'";
-	}
-
-	public void And(WhereClause wc) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " and ";
-	    }
-	    sql += " (" + wc.ToString() + ") ";
-	}
-
-	public void And(String field, Object value) {
-	    if (value is Int32) {
-		And(field, (Int32)value);
-	    } else if (value is DateTime) {
-		And(field, (DateTime)value);
-	    } else {
-		And(field, value.ToString().Replace("'", "''"));
-	    }
-	}
-
-	public void Or(String field, Int32 value) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " or ";
-	    }
-	    sql += field + "=" + value.ToString();
-	}
-
-	public void Or(String field, String value) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " or ";
-	    }
-	    sql += field + "='" + value.Replace("'", "''") + "'";
-	}
-
-	public void Or(String field, DateTime value) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " or ";
-	    }
-	    sql += field + "='" + value.ToString() + "'";
-	}
-
-	public void Or(WhereClause wc) {
-	    if (!sql.Equals(String.Empty)) {
-		sql += " or ";
-	    }
-	    sql += " (" + wc.ToString() + ") ";
-	}
-
-	public void Or(String field, Object value) {
-	    if (value is Int32) {
-		Or(field, (Int32)value);
-	    } else if (value is DateTime) {
-		Or(field, (DateTime)value);
-	    } else {
-		Or(field, value.ToString().Replace("'", "''"));
-	    }
-	}
-
+	/// <summary>
+	/// Returns true if nothing has been added to the clause; otherwise false.
+	/// </summary>
 	public Boolean IsEmpty {
 	    get { return sql.Equals(String.Empty); }
 	}
--- src/Test/WhereClauseTest.cs
+++ src/Test/WhereClauseTest.cs
@@ -19,7 +19,7 @@ namespace Spring2.Core.Test {
 	    
 	    // make sure that apostrophe is properly escaped
 	    filter = new WhereClause("column", "foo'bar");
-	    Assertion.Assert(filter.FormatSql(), filter.FormatSql().Equals(" where column='foo''bar'"));
+	    Assertion.AssertEquals(" WHERE column='foo''bar'", filter.FormatSql());
 	}
 
 	[Test]
@@ -27,7 +27,7 @@ namespace Spring2.Core.Test {
 	    // make sure that apostrophe is properly escaped
 	    WhereClause filter = new WhereClause("foo", "bar");
 	    filter.And("column", "foo'bar");
-	    Assertion.Assert(filter.FormatSql(), filter.FormatSql().Equals(" where foo='bar' and column='foo''bar'"));
+	    Assertion.AssertEquals(" WHERE foo='bar' AND column='foo''bar'", filter.FormatSql());
 	}
 	
 	[Test]
@@ -35,8 +35,158 @@ namespace Spring2.Core.Test {
 	    // make sure that apostrophe is properly escaped
 	    WhereClause filter = new WhereClause("foo", "bar");
 	    filter.Or("column", "foo'bar");
-	    Assertion.Assert(filter.FormatSql(), filter.FormatSql().Equals(" where foo='bar' or column='foo''bar'"));
+	    Assertion.AssertEquals(" WHERE foo='bar' OR column='foo''bar'", filter.FormatSql());
 	}
+
+	[Test]
+	public void Constructors() {
+	    WhereClause filter = new WhereClause();
+	    Assertion.Assert(filter.IsEmpty == true);
+	    
+	    filter = new WhereClause("columnA", 1);
+	    filter.And("columnB", 2);
+	    Assertion.AssertEquals(" WHERE columnA=1 AND columnB=2", filter.FormatSql());
+
+	    filter = new WhereClause("columnA", 1);
+	    filter.AndEquals("columnB", 2);
+	    Assertion.AssertEquals(" WHERE columnA=1 AND columnB=2", filter.FormatSql());
+	}
+
+	[Test]
+	public void And() {
+	    WhereClause filter = new WhereClause();
+
+	    filter = new WhereClause("columnA", 1);
+	    filter.And("columnB", 2);
+	    Assertion.AssertEquals(" WHERE columnA=1 AND columnB=2", filter.FormatSql());
+	}
+
+	[Test]
+	public void AndEquals() {
+	    WhereClause filter = new WhereClause();
+
+	    filter = new WhereClause("columnA", 1);
+	    filter.AndEquals("columnB", 2);
+	    Assertion.AssertEquals(" WHERE columnA=1 AND columnB=2", filter.FormatSql());
+	}
+
+	[Test]
+	public void AndBetween() {
+	    WhereClause filter = new WhereClause();
+
+	    filter = new WhereClause();
+	    filter.AndBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE columnA between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.AndBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE columnA between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.AndBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE columnA between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.AndBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE foo='bar' AND columnA between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.AndBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE foo='bar' AND columnA between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.AndBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE foo='bar' AND columnA between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+	}
+
+	[Test]
+	public void AndNotBetween() {
+	    WhereClause filter = new WhereClause();
+
+	    filter = new WhereClause();
+	    filter.AndNotBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE columnA NOT between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.AndNotBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE columnA NOT between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.AndNotBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE columnA NOT between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.AndNotBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE foo='bar' AND columnA NOT between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.AndNotBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE foo='bar' AND columnA NOT between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.AndNotBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE foo='bar' AND columnA NOT between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+	}
+
+	[Test]
+	public void OrBetween() {
+	    WhereClause filter = new WhereClause();
+
+	    filter = new WhereClause();
+	    filter.OrBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE columnA between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.OrBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE columnA between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.OrBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE columnA between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.OrBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE foo='bar' OR columnA between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.OrBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE foo='bar' OR columnA between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.OrBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE foo='bar' OR columnA between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+	}
+
+	[Test]
+	public void OrNotBetween() {
+	    WhereClause filter = new WhereClause();
+
+	    filter = new WhereClause();
+	    filter.OrNotBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE columnA NOT between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.OrNotBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE columnA NOT between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause();
+	    filter.OrNotBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE columnA NOT between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.OrNotBetween("columnA", 1, 2);
+	    Assertion.AssertEquals(" WHERE foo='bar' OR columnA NOT between 1 and 2", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.OrNotBetween("columnA", "1", "2");
+	    Assertion.AssertEquals(" WHERE foo='bar' OR columnA NOT between '1' and '2'", filter.FormatSql());
+
+	    filter = new WhereClause("foo", "bar");
+	    filter.OrNotBetween("columnA", new DateTime(1969, 12, 18), new DateTime(2000, 2, 8));
+	    Assertion.AssertEquals(" WHERE foo='bar' OR columnA NOT between '12/18/1969 12:00:00 AM' and '2/8/2000 12:00:00 AM'", filter.FormatSql());
+	}
+
 
     }
 }
