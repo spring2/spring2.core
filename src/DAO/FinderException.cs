348f15548f948b2bae0f3192b2b162a03fe73eb2
cort <none@none>
2003-04-22 22:13:22 +0000

    derive from Spring2.Core.DaoException

--- src/DAO/FinderException.cs
+++ src/DAO/FinderException.cs
@@ -5,7 +5,7 @@ namespace Spring2.Core.DAO {
     /// <summary>
     /// Exception created when a finder returns no rows.
     /// </summary>
-    public class FinderException : System.ApplicationException {
+    public class FinderException : DaoException {
 	public FinderException() {}
 
 	public FinderException (String message) : base(message) {}
