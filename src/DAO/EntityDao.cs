fce2aa321e77939b28aafead9c5ead7aa5ad934b
cort <none@none>
2004-01-08 23:34:11 +0000

    add support to GetListReader to get TOP x rows add support using DaoTransaction for manual transaction

--- src/DAO/EntityDao.cs
+++ src/DAO/EntityDao.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Specialized;
 using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
@@ -6,47 +7,96 @@ using System.Collections;
 
 using Microsoft.Win32;
 
+using Spring2.Core.DAO;
+using Spring2.Core.Util;
+
 namespace Spring2.Core.DAO {
  
     public abstract class EntityDAO { 
 
+	private static StringDictionary connectionStrings = new StringDictionary();
 	protected static SqlDataReader GetListReader(String key, String viewName) { 
 	    return GetListReader(key, viewName, null, null); 
 	} 
 		 
 	protected static SqlDataReader GetListReader(String key, String viewName, IWhere whereClause, IOrderBy orderByClause) { 
-	    try { 
-		String sql = "select * from " + viewName;
-		if (whereClause != null) { 
-		    sql = sql + whereClause.FormatSql(); 
-		} 
-		if (orderByClause != null) { 
-		    sql = sql + orderByClause.FormatSql(); 
-		} 
-			 
-		return ExecuteReader(key, sql);
-	    } catch (Exception ex) { 
-		// log exception
-		throw ex;
+	    String sql = "select * from " + viewName;
+	    if (whereClause != null) { 
+		sql = sql + whereClause.FormatSql(); 
 	    } 
+	    if (orderByClause != null) { 
+		sql = sql + orderByClause.FormatSql(); 
+	    } 
+			 
+	    return ExecuteReader(key, sql);
+	}
+
+	protected static SqlDataReader GetListReader(String key, String viewName, IWhere whereClause, IOrderBy orderByClause, Int32 maxRows) { 
+	    String sql = "select top " + maxRows.ToString() + " * from " + viewName;
+	    if (whereClause != null) { 
+		sql = sql + whereClause.FormatSql(); 
+	    } 
+	    if (orderByClause != null) { 
+		sql = sql + orderByClause.FormatSql(); 
+	    } 
+			 
+	    return ExecuteReader(key, sql);
 	}
 
 	protected static SqlDataReader ExecuteReader(String key, String sql) { 
 	    SqlCommand cmd = GetSqlCommand(key);
+	    cmd.CommandType = CommandType.Text;
+	    cmd.CommandText = sql;
 			 
 	    try { 
-		cmd.CommandType = CommandType.Text;
-		cmd.CommandText = sql;
-		return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+		SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+		return reader;
 	    } catch (Exception ex) { 
-		// log exception
-		throw ex;
-	    } 
+		throw new DaoException(ex.Message + " [running statement: " + sql + "]", ex);
+	    }
 	}
 
-	protected static SqlCommand GetSqlCommand(String key) {
-	    // determine where the connection string comes from and what it is
+	protected static SqlCommand GetSqlCommand(String key, SqlTransaction transaction) {
+	    Timer timer = new Timer();	
+
+	    String connectionString = GetConnectionString(key);
+	    
+	    if (transaction != null) {
+		// for now, since there is no functionality for enlisting a connection in a distributed transaction,
+		// throw an exception if the connection string is not the same
+		//TODO: this will not be the same if there is a password in it
+		//		if (!transaction.Connection.ConnectionString.Equals(key)) {
+		//		    throw new DaoException("Connection string from transaction (" + transaction.Connection.ConnectionString + ") does not match connection string (" + connectionString + ") retrieved by key (" + key + ")");
+		//		}
+
+		SqlCommand cmd = new SqlCommand();
+		cmd.Connection = transaction.Connection;
+		cmd.Transaction = transaction;
+
+		return cmd;
+	    } else {
+		SqlCommand cmd = GetSqlCommand(key);
+		// TODO: System.EnterpriseServices.ITransaction is needed to enlist a connection in a DTC transaction
+		//if (transaction != null) {
+		//    cmd.Connection.EnlistDistributedTransaction(transaction);
+		//}
+		return cmd;
+	    }
+	}
+
+	/// <summary>
+	/// Determine where the connection string comes from and what it is
+	/// </summary>
+	/// <param name="key"></param>
+	/// <returns></returns>
+	protected static String GetConnectionString(String key) {
 	    String connectionString;
+
+	    // try cache first
+	    if (connectionStrings[key]!= null) {
+		return connectionStrings[key];
+	    }
+
 	    if (key.ToLower().StartsWith("registry")) {
 		String subkey = key.Substring(key.LastIndexOf(":")+1);
 		subkey = subkey.Substring(0, subkey.LastIndexOf("\\"));
@@ -66,15 +116,28 @@ namespace Spring2.Core.DAO {
 		connectionString = rkey.GetValue(value).ToString();
 	    } else {
 		connectionString = ConfigurationSettings.AppSettings[key];
-		if (connectionString == null) {
-		    throw new ConfigurationException("No app setting found for key: " + key + ".  Check Web.config for this value.");
-		}
 	    }
 
 	    connectionString = connectionString.ToUpper();
 	    connectionString = connectionString.Replace("NETWORK=DBMSSOCN;", "Network Library=DBMSSOCN;");
 	    connectionString = connectionString.Replace("PROVIDER=MSDASQL;", "");
 	    connectionString = connectionString.Replace("DRIVER=SQL SERVER;", "");
+
+	    // cache the connection string by key for fast lookup later
+	    connectionStrings.Add(key, connectionString);
+
+	    return connectionString;
+	}
+
+	protected static SqlConnection GetSqlConnection(String connectionString) {
+	    SqlConnection conn = new SqlConnection(connectionString);
+	    conn.Open();
+
+	    return conn;
+	}
+
+	protected static SqlCommand GetSqlCommand(String key) {
+	    String connectionString = GetConnectionString(key);
 
 	    SqlConnection conn = new SqlConnection(connectionString);
 	    conn.Open();
@@ -94,6 +157,14 @@ namespace Spring2.Core.DAO {
 
 	protected static SqlCommand GetSqlCommand(String key, String commandText, CommandType commandType, Int32 commandTimeout) {
 	    SqlCommand cmd = GetSqlCommand(key);
+	    cmd.CommandText = commandText;
+	    cmd.CommandType = commandType;
+	    cmd.CommandTimeout = commandTimeout;
+	    return cmd;
+	}
+
+	protected static SqlCommand GetSqlCommand(String key, String commandText, CommandType commandType, Int32 commandTimeout, SqlTransaction transaction) {
+	    SqlCommand cmd = GetSqlCommand(key, transaction);
 	    cmd.CommandText = commandText;
 	    cmd.CommandType = commandType;
 	    cmd.CommandTimeout = commandTimeout;
