20cb121721cb4ebf5a16cacd3b54c39149a52012
cort <none@none>
2004-07-29 13:14:19 +0000

    updated version to match the version of NVelocity that has had bug fixes

--- src/IO/ResourceLocator.cs
+++ src/IO/ResourceLocator.cs
@@ -17,17 +17,23 @@ namespace Spring2.Core.IO {
 	private IList assemblies = new ArrayList();
 	private Assembly assembly = null;
 
-	public ResourceLocator(String filename) {
-	    this.filename = filename;
+	public ResourceLocator(String filename) : this(null, filename) {}
+
+	public ResourceLocator(String path, String filename) {
+	    if (path != null) {
+		this.filename = path + "\\" + filename;
+	    } else {
+		this.filename = filename;
+	    }
 
 	    file = new FileInfo(this.filename);
 	    if (file.Exists) {
 		this.filename = file.FullName;
 		isFile = true;
 	    } else {
-		foreach(Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
-		    GetReferencedAssemblies(assembly, assemblies);
-		}
+		// the Calling, Executing and Entry assemblies may not have links to each other -
+		// so attempt to get assemblies from all possible paths.  The current AppDomain may also
+		// know of more assemblies.
 		GetReferencedAssemblies(System.Reflection.Assembly.GetExecutingAssembly(), assemblies);
 		if (System.Reflection.Assembly.GetEntryAssembly() != null) {
 		    GetReferencedAssemblies(System.Reflection.Assembly.GetEntryAssembly(), assemblies);
@@ -35,18 +41,24 @@ namespace Spring2.Core.IO {
 		if (System.Reflection.Assembly.GetCallingAssembly() != null) {
 		    GetReferencedAssemblies(System.Reflection.Assembly.GetCallingAssembly(), assemblies);
 		}
+		foreach(Assembly assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+		    GetReferencedAssemblies(assembly, assemblies);
+		}
 
-		String fn = filename.ToLower().Replace("\\", ".");
+		String fn = filename.ToLower().Replace(".\\", "");
+		fn = fn.Replace("\\", ".");
+		fn = fn.Replace("/", ".");
 		foreach(Assembly a in assemblies) {
-//Console.Out.WriteLine(a.FullName);
-		    String prefix = a.FullName.Substring(0,a.FullName.IndexOf(",")).ToLower();
-		    String[] names = a.GetManifestResourceNames();
-		    foreach(String s in names) {
-//Console.Out.WriteLine(prefix + " :: " + s.ToLower() + " :: " + fn);
-			if (s.ToLower().Equals(fn) || s.ToLower().Equals(prefix + "." + fn)) {
-			    this.filename = s;
-			    assembly = a;
-			    isResource = true;
+		    // don't attempt to find resources in dynamic assemblies as it is not supported
+		    if (!(a is System.Reflection.Emit.AssemblyBuilder)) {
+			String prefix = a.FullName.Substring(0,a.FullName.IndexOf(",")).ToLower();
+			String[] names = a.GetManifestResourceNames();
+			foreach(String s in names) {
+			    if (s.ToLower().Equals(fn) || s.ToLower().Equals(prefix + "." + fn)) {
+				this.filename = s;
+				assembly = a;
+				isResource = true;
+			    }
 			}
 		    }
 		}
@@ -89,6 +101,32 @@ namespace Spring2.Core.IO {
 	    get { return (isFile || isResource); }
 	}
 
+	private Stream FindResource(String path, String template) {
+	    try {
+		FileInfo file = new FileInfo(path + "\\" + template)
+		    ;
+
+		if (file.Exists) {
+		    return new BufferedStream(new FileStream(file.FullName, FileMode.Open, FileAccess.Read));
+		} else {
+		    IList list = new ArrayList();
+		    GetReferencedAssemblies(System.Reflection.Assembly.GetEntryAssembly(), list);
+
+		    foreach(Assembly a in list) {
+			System.IO.Stream s = a.GetManifestResourceStream(template)
+			    ;
+			if (s != null) {
+			    return s;
+			}
+		    }
+
+
+		    return null;
+		}
+	    } catch (System.IO.FileNotFoundException) {
+		return null;
+	    }
+	}
 
     }
 }
