fe894de0e24a0b9bda90aab0581972f1c01e9922
crandall <none@none>
2004-10-12 17:24:45 +0000

    Extend base entity dao class.

--- src/DAO/OracleEntityDAO.cs
+++ src/DAO/OracleEntityDAO.cs
@@ -1,67 +1,58 @@
 using System;
-using System.Configuration;
 using System.Data;
 using System.Data.OracleClient;
 
 namespace Spring2.Core.DAO {
  
-    public abstract class OracleEntityDAO { 
+    public class OracleEntityDAO : BaseEntityDAO { 
 
-	protected static IDataReader GetListReader(String key, String viewName) { 
-	    return GetListReader(key, viewName, null, null); 
-	} 
-		 
-	protected static IDataReader GetListReader(String key, String viewName, IWhere whereClause, IOrderBy orderByClause) { 
+	protected override IDbCommand GetDbCommand(String key, IDbTransaction transaction) {
 
-	    try { 
-		String sql = "select * from " + viewName;
-		if (whereClause != null) { 
-		    sql = sql + whereClause.FormatSql(); 
-		} 
-		if (orderByClause != null) { 
-		    sql = sql + orderByClause.FormatSql(); 
-		} 
-			 
-		return ExecuteReader(key, sql);
-	    } catch (Exception ex) { 
-		// log exception
-		throw ex;
-	    } 
-	}
-
-	protected static IDataReader ExecuteReader(String key, String sql) { 
-
-	    IDbCommand cmd = GetCommand(key);			 
-	    try { 
-		cmd.CommandType = CommandType.Text;
-		cmd.CommandText = sql;
-		return cmd.ExecuteReader(CommandBehavior.CloseConnection);
-	    } catch (Exception ex) { 
-		// log exception
-		throw ex;
-	    } 
-	}
-
-	protected static IDbCommand GetCommand(String key) {
-
-	    String connectionString = ConfigurationSettings.AppSettings[key];
-	    if (connectionString == null) {
-		throw new ConfigurationException("No app setting found for key: " + key + ".  Check Web.config for this value.");
+	    String connectionString = GetConnectionString(key);
+	    
+	    if (transaction == null) {
+		return GetDbCommand(key);
+	    } else {
+		IDbCommand cmd = new OracleCommand();
+		cmd.Connection = transaction.Connection;
+		cmd.Transaction = transaction;
+		return cmd;
 	    }
+	}
+
+	protected override IDbCommand GetDbCommand(String key) {
+	    String connectionString = GetConnectionString(key);	    
 	    OracleConnection conn = new OracleConnection(connectionString);
 	    conn.Open();
 	    OracleCommand cmd = new OracleCommand();
 	    cmd.Connection = conn;
-
+	    
 	    return cmd;
 	}
 
-	protected static IDbCommand GetCommand(String key, String commandText, CommandType commandType) {
+	protected override IDbDataParameter CreateDataParameter(String parameterName, ParameterDirection direction, Object value) {
+	    OracleParameter param = new OracleParameter(parameterName, value);
+	    param.Direction = direction;
+	    return param;
+	}
 
-	    IDbCommand cmd = GetCommand(key);
-	    cmd.CommandText = commandText;
-	    cmd.CommandType = commandType;
-	    return cmd;
+	protected override IDbDataParameter CreateDataParameter(String parameterName, DbType dbType, ParameterDirection direction) {
+	    OracleParameter param = new OracleParameter(parameterName, OracleType.NVarChar);
+	    param.DbType = dbType;
+	    param.Direction = direction;
+	    return param;
+	}
+
+	protected override IDbDataParameter CreateDataParameter(String parameterName, DbType dbType, Int32 size, ParameterDirection direction, Boolean isNullable, Byte precision, Byte scale, String sourceColumn, DataRowVersion sourceVersion, Object value) {
+	    OracleParameter param = new OracleParameter(parameterName, OracleType.NVarChar, size, direction, isNullable, precision, scale, sourceColumn, sourceVersion, value);
+	    param.DbType = dbType;
+	    return param;
+	}
+
+	protected override IDbConnection GetDbConnection(String connectionString) {
+	    OracleConnection conn = new OracleConnection(connectionString);
+	    conn.Open();
+	    return conn;
 	}
     }
 }
