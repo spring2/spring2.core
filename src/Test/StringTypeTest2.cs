984c4a76246f284c466129734fd38e2ea86c8bfe
cort <none@none>
2005-04-19 22:48:31 +0000

    test to make sure that ToString returns UNSET and DEFAULT when not valid an does not throw an exception

--- src/Test/StringTypeTest2.cs
+++ src/Test/StringTypeTest2.cs
@@ -19,6 +19,7 @@ namespace Spring2.Core.Test {
 	    Assert.AreEqual(StringType.Parse("foo"), s1);
 	}
 
+	[Test]
 	public void AssignementFromObject() {
 	    StringType s1 = "foo";
 	    Object o = s1 as Object;
@@ -28,10 +29,57 @@ namespace Spring2.Core.Test {
 	    Assert.AreEqual(s1, propertyValue);
 	}
 
+	/// <summary>
+	/// Property for AssignementFromDataObject test
+	/// </summary>
 	public StringType StringTypeProperty {
 	    get { return propertyValue; }
 	    set { propertyValue = value; }
 	}
 
+	[Test]
+	public void ToStringTest() {
+	    Assert.AreEqual("UNSET", StringType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", StringType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", BooleanType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", BooleanType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", CurrencyType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", CurrencyType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", DateType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", DateType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", DecimalType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", DecimalType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", GenderType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", GenderType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", IdType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", IdType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", IntegerType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", IntegerType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", NumberType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", NumberType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", PhoneNumberType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", PhoneNumberType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", QuantityType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", QuantityType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", RowVersionType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", RowVersionType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", StringType.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", StringType.DEFAULT.ToString());
+
+	    Assert.AreEqual("UNSET", USStateCodeEnum.UNSET.ToString());
+	    Assert.AreEqual("DEFAULT", USStateCodeEnum.DEFAULT.ToString());
+	}
     }
 }
