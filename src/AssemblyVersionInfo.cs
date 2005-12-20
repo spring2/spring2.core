c1d996554e4d9a065b91a2e5148b0fbf63dcc003
cort <none@none>
2005-12-20 14:18:28 +0000

    update version

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -9,4 +9,4 @@
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: System.Reflection.AssemblyVersion("1.1.12.3154")]
+[assembly: System.Reflection.AssemblyVersion("1.1.13.2")]
