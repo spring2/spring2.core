c984d7a20ee1fdcb64bad9551b7ef6ce74428bab
cort <none@none>
2006-01-13 17:10:06 +0000

    updates to support compiling both 1.1 and 2.0 framework versions of Spring2.Core.
    Wahoo!

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
+[assembly: System.CLSCompliant(false)]
+
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
