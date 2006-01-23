46ccdf0c4f14f9e72773895ec3afc36c49fd65b4
kend <none@none>
2006-01-23 18:12:14 +0000

    Provider for TeleAtlas geocoding service

---
+++ src/Geocode/TeleAtlasProvider.cs
@@ -0,0 +1,22 @@
+using System;
+using Spring2.Core.Types;
+
+namespace Spring2.Core.Geocode {
+    /// <summary>
+    /// Summary description for TeleAtlasProvider.
+    /// </summary>
+    public class TeleAtlasProvider : IGeocodeProvider {
+	public TeleAtlasProvider() {
+	}
+
+	public GeocodeData GetCityAndStateOfZipCode(StringType zipCode) {
+	    GeocodeWrapper wrapper = new GeocodeWrapper();
+	    return wrapper.GetCityAndStateOfZipCode(zipCode);
+	}
+
+	public GeocodeData DoGeocode(StringType street, StringType city, StringType state, StringType postalCode, StringType path) {
+	    GeocodeWrapper wrapper = new GeocodeWrapper();
+	    return wrapper.DoGeocode(street, city, state, postalCode, path);
+	}
+    }
+}
