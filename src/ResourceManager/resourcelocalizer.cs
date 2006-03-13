5affdc15e0c17e2f58e3ef5c14ae29c59c81005b
nathanp <none@none>
2006-03-13 15:10:57 +0000

    Moved the 'default' resource finder to another method to make it optional

--- src/ResourceManager/resourcelocalizer.cs
+++ src/ResourceManager/resourcelocalizer.cs
@@ -27,6 +27,57 @@ namespace Spring2.Core.ResourceManager {
 	    try{
 		Resource resource;
 		if (identity.IsValid){
+		    resource = ResourceDAO.DAO.FindByContextFieldAndIdentity(context, field, identity);
+		}else{
+		    resource = ResourceDAO.DAO.FindByContextFieldAndIdentity(context, field, IdType.UNSET);
+		}
+		//now take that resource to the localized resource table
+		LocalizedResource localizedResource = LocalizedResourceDAO.DAO.FindByResourceIdLocaleAndLanguage(resource.ResourceId, locale, language);
+		return localizedResource.Content;
+
+	    }catch (FinderException){
+		String unknownResouce = context.ToString();
+		if (identity.IsValid){
+		    unknownResouce += "(" + identity.ToInt32().ToString() + ")";
+		}
+		unknownResouce += "." + field.ToString();
+		return StringType.Parse(unknownResouce);
+	    }								       
+
+	}
+
+
+	/// <summary>
+	/// Looks up a localized resource using an identity, but returns the non identity specific version if none is found
+	/// Uses the locale of the current user to find the appropriate resource
+	/// </summary>
+	/// <param name="context"></param>
+	/// <param name="field"></param>
+	/// <param name="identity"></param>
+	/// <returns></returns>
+	public StringType LocalizeWithDefault(StringType context, StringType field, IdType identity){
+	    if (System.Threading.Thread.CurrentPrincipal is ISpring2Principal) {
+		ISpring2Principal currentPrincipal = (ISpring2Principal)System.Threading.Thread.CurrentPrincipal;
+		return LocalizeWithDefault(context, field, identity, currentPrincipal.Locale, currentPrincipal.Language);
+	    } else {
+		throw new ApplicationException("Principal on thread is not the correct type");
+	    }
+	}
+
+	/// <summary>
+	/// Looks up a localized resource using an identity, but returns the non identity specific version if none is found
+	/// </summary>
+	/// <param name="context"></param>
+	/// <param name="field"></param>
+	/// <param name="identity"></param>
+	/// <param name="locale"></param>
+	/// <param name="language"></param>
+	/// <returns></returns>
+	public StringType LocalizeWithDefault(StringType context, StringType field, IdType identity, ILocale locale, ILanguage language){
+	    //First, look up the resource id using the context, key, and identity.
+	    try{
+		Resource resource;
+		if (identity.IsValid){
 		    try{
 			//try finding the identity specific version of this resource
 			resource = ResourceDAO.DAO.FindByContextFieldAndIdentity(context, field, identity);
