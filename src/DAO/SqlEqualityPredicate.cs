2925952f2bc63b7847bf6057558d93927f78cf03
cort <none@none>
2006-06-19 22:28:39 +0000

    added constructor that takes object and then checks for the explicit types.  This was to handle the dataType.ToXxx as Object : DBNull.Value syntax in the dao classes.

--- src/DAO/SqlEqualityPredicate.cs
+++ src/DAO/SqlEqualityPredicate.cs
@@ -14,96 +14,95 @@ namespace Spring2.Core.DAO {
     	private SqlDbType dbType;
     		
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Int64 value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.BigInt;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.BigInt);
 	}
     	
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Int32 value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.Int;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Int);
 	}
 
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Int16 value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.SmallInt;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.SmallInt);
 	}
     	
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, DateTime value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.DateTime;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.DateTime);
 	}
 
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, String value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.VarChar;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.VarChar);
 	}
 
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Decimal value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.Decimal;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Decimal);
 	}
 
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Double value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.Float;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Float);
 	}
 
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Single value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.Real;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Real);
 	}
     	
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Boolean value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.Bit;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Bit);
 	}
     	
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Byte value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.TinyInt;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.TinyInt);
 	}
     	
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Byte[] value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.Image;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Image);
 	}
     	
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Guid value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = value;
-	    dbType = SqlDbType.UniqueIdentifier;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.UniqueIdentifier);
 	}
 
 	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, DBNull value) {
-	    this.columnName = columnName;
-	    this._operator = _operator;
-	    this.value = null;
-	    dbType = SqlDbType.Variant;
+	    SetInternalValues(columnName, _operator, value, SqlDbType.Variant);
 	}
 
+	public SqlEqualityPredicate(String columnName, EqualityOperatorEnum _operator, Object value) {
+	    if (value is DBNull) {
+		SetInternalValues(columnName, _operator, value as DBNull, SqlDbType.Variant);
+	    } else if (value is Int64) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.BigInt);
+	    } else if (value is Int32) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.Int);
+	    } else if (value is Int16) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.SmallInt);
+	    } else if (value is DateTime) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.DateTime);
+	    } else if (value is String) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.VarChar);
+	    } else if (value is Decimal) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.Decimal);
+	    } else if (value is Double) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.Float);
+	    } else if (value is Single) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.Real);
+	    } else if (value is Boolean) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.Bit);
+	    } else if (value is Byte) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.TinyInt);
+	    } else if (value is Byte[]) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.Image);
+	    } else if (value is Guid) {
+		SetInternalValues(columnName, _operator, value, SqlDbType.UniqueIdentifier);
+	    } else {
+	    	throw new ArgumentException(value.GetType().ToString() + " is not an allowed type");
+	    }
+	}		
+    	
+    	private void SetInternalValues(String columnName, EqualityOperatorEnum _operator, Object value, SqlDbType dbType) {
+	    this.columnName = columnName;
+	    this._operator = _operator;
+	    this.value = value;
+	    this.dbType = dbType;
+    	}
 
 	public override String Expression {
 	    get {
