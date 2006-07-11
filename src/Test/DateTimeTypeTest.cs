f4e56e6808ee95959efdcd39525a330741622488
crandall <none@none>
2006-07-11 20:02:21 +0000

    Add tests for all constructors.

--- src/Test/DateTimeTypeTest.cs
+++ src/Test/DateTimeTypeTest.cs
@@ -20,5 +20,101 @@ namespace Spring2.Core.Test {
 	    d2 = DateTimeType.Now.AddSeconds(-240);
 	    Assert.AreEqual(1, d1.CompareNoSeconds(d2));
 	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithTicks() {
+	    DateTimeType date = new DateTimeType(630823790450060000);
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(3, date.Hour);
+	    Assert.AreEqual(4, date.Minute);
+	    Assert.AreEqual(5, date.Second);
+	    Assert.AreEqual(6, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithYearMonthDay() {
+	    DateTimeType date = new DateTimeType(2000, 1, 2);
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(0, date.Hour);
+	    Assert.AreEqual(0, date.Minute);
+	    Assert.AreEqual(0, date.Second);
+	    Assert.AreEqual(0, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithYearMonthDayCalendar() {
+	    DateTimeType date = new DateTimeType(2000, 1, 2, new System.Globalization.GregorianCalendar());
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(0, date.Hour);
+	    Assert.AreEqual(0, date.Minute);
+	    Assert.AreEqual(0, date.Second);
+	    Assert.AreEqual(0, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithYearMonthDayHourMinutesSeconds() {
+	    DateTimeType date = new DateTimeType(2000, 1, 2, 3, 4, 5);
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(3, date.Hour);
+	    Assert.AreEqual(4, date.Minute);
+	    Assert.AreEqual(5, date.Second);
+	    Assert.AreEqual(0, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithYearMonthDayHourMinutesSecondsCalendar() {
+	    DateTimeType date = new DateTimeType(2000, 1, 2, 3, 4, 5, new System.Globalization.GregorianCalendar());
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(3, date.Hour);
+	    Assert.AreEqual(4, date.Minute);
+	    Assert.AreEqual(5, date.Second);
+	    Assert.AreEqual(0, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithYearMonthDayHourMinutesSecondsMilliseconds() {
+	    DateTimeType date = new DateTimeType(2000, 1, 2, 3, 4, 5, 6);
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(3, date.Hour);
+	    Assert.AreEqual(4, date.Minute);
+	    Assert.AreEqual(5, date.Second);
+	    Assert.AreEqual(6, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateWithYearMonthDayHourMinutesSecondsMillisecondsCalendar() {
+	    DateTimeType date = new DateTimeType(2000, 1, 2, 3, 4, 5, 6, new System.Globalization.GregorianCalendar());
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(3, date.Hour);
+	    Assert.AreEqual(4, date.Minute);
+	    Assert.AreEqual(5, date.Second);
+	    Assert.AreEqual(6, date.Millisecond);
+	}
+
+	[Test]
+	public void ShouldBeAbleToCreateFromDateTime() {
+	    DateTimeType date = new DateTimeType(new DateTime(2000, 1, 2, 3, 4, 5, 6));
+	    Assert.AreEqual(2000, date.Year);
+	    Assert.AreEqual(1, date.Month);
+	    Assert.AreEqual(2, date.Day);
+	    Assert.AreEqual(3, date.Hour);
+	    Assert.AreEqual(4, date.Minute);
+	    Assert.AreEqual(5, date.Second);
+	    Assert.AreEqual(6, date.Millisecond);
+	}
     }
 }
