6a50b79eb397932d863844b157f8890c68fb25f9
cort <none@none>
2006-07-11 19:38:48 +0000

    cleaned out remaining old style sql filter code

--- src/DAO/DatabaseFilter.cs
+++ src/DAO/DatabaseFilter.cs
@@ -5,242 +5,98 @@ using System.Data;
 namespace Spring2.Core.DAO {
 
     /// <summary>
-    /// Summary description for Filter.
+    /// Base class for database filters that offers the ability to create query fragments without having to know the
+    /// specific database or concatinate strings.
+    /// 
+    /// Supports parameterized fragments.
     /// </summary>
-    public abstract class DatabaseFilter {
+    public abstract class DatabaseFilter : IDatabaseExpression {
 
-	/// <summary>
-	/// This is how individual fragments are joined together
-	/// </summary>
-	/// <remarks>And and Or are added to support nested Filters</remarks>
-	internal enum FilterConjunction {
-	    And,
-	    Or,
-	    AndEquals,
-	    AndNotEquals,
-	    OrEquals,
-	    OrNotEquals,
-	    AndBetween,
-	    AndNotBetween,
-	    OrBetween,
-	    OrNotBetween,
-	    AndIn,
-	    AndNotIn,
-	    OrIn,
-	    OrNotIn,
-	    AndLike,
-	    AndNotLike,
-            OrLike,
-	    OrNotLike
-	}
-
-	/// <summary>
-	/// Structure to hold filter fragments
-	/// </summary>
-	internal class FilterClause {
-	    private FilterConjunction conjunction;
-	    private String key;
-	    private Object value;
-	    private Object[] values = new Object[0];
-
-	    public FilterClause(FilterConjunction conjunction, String key, Object value) {
-		this.conjunction = conjunction;
-		this.key = key;
-		if (value is DBNull) {
-	    	    this.value = null;
-		} else {
-		    this.value = value;
-		}
+	protected struct LogicalExpression {
+	    public LogicalOperatorEnum Operator;
+	    public IDatabaseExpression Expression;
+    		
+	    public LogicalExpression(IDatabaseExpression expression) {
+		Expression = expression;
+		Operator = LogicalOperatorEnum.None;
 	    }
-
-	    public FilterClause(FilterConjunction conjunction, String key, Object value1, Object value2) {
-		this.conjunction = conjunction;
-		this.key = key;
-	    	this.values = new Object[2] {value1, value2};
-	    }
-
-	    public FilterClause(FilterConjunction conjunction, String key, Object[] values) {
-		this.conjunction = conjunction;
-		this.key = key;
-		this.values = values;
-	    }
-
-	    public FilterConjunction Conjunction {
-	    	get { return conjunction; }
-	    }
-
-	    public String Key {
-		get { return key; }
-	    }
-	    
-	    public Object Value {
-		get { return value; }
-	    }
-
-	    public Object[] Values {
-		get { return values; }
+	    public LogicalExpression(LogicalOperatorEnum op, IDatabaseExpression expression) {
+		Expression = expression;
+		Operator = op;
 	    }
 	}
 
-	private ArrayList fragments = new ArrayList();
-    	
-	#region Constructors
+    	protected ArrayList expressions = new ArrayList();
 
-	public DatabaseFilter() {
-	}
-
-	public DatabaseFilter(String expression) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndEquals, null, expression));
-	}
-    	
-    	public DatabaseFilter(DatabaseFilter filter) {
-    	    fragments.AddRange(filter.Fragments);
-    	}
-
-    	public DatabaseFilter(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndEquals, key, value));
-	}
-
-//	public Filter(String key, DataType value) {
-//	    fragments.Add(new FilterClause(FilterConjunction.AndEquals, key, value.DBValue));
-//	}
-	#endregion
-    	
-	#region And
-	public void AndEquals(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndEquals, key, value));
-	}
-
-	public void AndNotEquals(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndNotEquals, key, value));
-	}
-	
-//	public void AndEquals(String key, DataType value) {
-//	    fragments.Add(new FilterClause(FilterConjunction.AndEquals, key, value.DBValue));
-//	}
-
-    	public void And(DatabaseFilter filter) {
-	    fragments.Add(new FilterClause(FilterConjunction.And, null, filter));
-	}
-
-	public void And(String value) {
-	    fragments.Add(new FilterClause(FilterConjunction.And, null, value));
-	}
-	#endregion
-
-	#region Or
-	public void OrEquals(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.OrEquals, key, value));
-	}
-	#endregion
-    	
-	#region Like
-	public void AndLike(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndLike, key, value));
-	}
-	public void OrLike(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.OrLike, key, value));
-	}
-
-	public void AndNotLike(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndNotLike, key, value));
-	}
-	public void OrNotLike(String key, Object value) {
-	    fragments.Add(new FilterClause(FilterConjunction.OrNotLike, key, value));
-	}
-	#endregion
-    	
-	#region Between
-	public void AndBetween(String key, Object value1, Object value2) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndBetween, key, value1, value2));
-	}
-	public void OrBetween(String key, Object value1, Object value2) {
-	    fragments.Add(new FilterClause(FilterConjunction.OrBetween, key, value1, value2));
-	}
-
-	public void AndNotBetween(String key, Object value1, Object value2) {
-	    fragments.Add(new FilterClause(FilterConjunction.AndNotBetween, key, value1, value2));
-	}
-	public void OrNotBetween(String key, Object value1, Object value2) {
-	    fragments.Add(new FilterClause(FilterConjunction.OrNotBetween, key, value1, value2));
-	}
-	#endregion
-    	
-	#region In
-	public void AndIn(String key, Object[] values) {
-	    if (values.Length==0) {
-		throw new ArgumentException("Values must contain at least 1 value", "values");
-	    }
-	    fragments.Add(new FilterClause(FilterConjunction.AndIn, key, values));
-	}
-	public void OrIn(String key, Object[] values) {
-	    if (values.Length==0) {
-		throw new ArgumentException("Values must contain at least 1 value", "values");
-	    }
-	    fragments.Add(new FilterClause(FilterConjunction.OrIn, key, values));
-	}
-
-	public void AndNotIn(String key, Object[] values) {
-	    if (values.Length==0) {
-		throw new ArgumentException("Values must contain at least 1 value", "values");
-	    }
-	    fragments.Add(new FilterClause(FilterConjunction.AndNotIn, key, values));
-	}
-	public void OrNotIn(String key, Object[] values) {
-	    if (values.Length==0) {
-		throw new ArgumentException("Values must contain at least 1 value", "values");
-	    }
-	    fragments.Add(new FilterClause(FilterConjunction.OrNotIn, key, values));
-	}
-	#endregion
-    	
-	#region Abstract
-	public abstract String InnerStatement {
+    	/// <summary>
+    	/// This is the independent fragment based contained expressions.
+    	/// </summary>
+	public abstract String Expression {
 	    get;
 	}
 
-	public abstract String Statement {
+    	/// <summary>
+    	/// This is the complete WHERE fragment based on the contained expressions.
+    	/// </summary>
+    	public abstract String Statement {
 	    get;
 	}
 
+    	/// <summary>
+    	/// Collection of data parameters to be added to the command when executed.
+    	/// </summary>
 	public abstract IDataParameterCollection Parameters {
 	    get;
 	}
-	#endregion
     	
-	internal ArrayList Fragments {
-	    get { return fragments; }	
+    	/// <summary>
+    	/// Adds an expression and logically ands this with the existing contained expressions.
+    	/// </summary>
+    	/// <param name="expression"></param>
+    	/// <returns></returns>
+	public IDatabaseExpression And(IDatabaseExpression expression) {
+	    if (expressions.Count==0) {
+		expressions.Add(new LogicalExpression(expression));
+	    } else {
+		expressions.Add(new LogicalExpression(LogicalOperatorEnum.And, expression));
+	    }
+	    return this;
 	}
-    	
+
 	/// <summary>
+	/// Adds an expression and logically ors this with the existing contained expressions.
+	/// </summary>
+	/// <param name="expression"></param>
+	/// <returns></returns>
+	public IDatabaseExpression Or(IDatabaseExpression expression) {
+	    if (expressions.Count==0) {
+		expressions.Add(new LogicalExpression(expression));
+	    } else {
+		expressions.Add(new LogicalExpression(LogicalOperatorEnum.Or, expression));
+	    }
+	    return this;
+	}
+
+	/// <summary>
+	/// Adds an expression and logically ands the notted result with the existing contained expressions.
+	/// </summary>
+	/// <param name="expression"></param>
+	/// <returns></returns>
+	public IDatabaseExpression Not(IDatabaseExpression expression) {
+	    if (expressions.Count==0) {
+		expressions.Add(new LogicalExpression(expression));
+	    } else {
+		expressions.Add(new LogicalExpression(LogicalOperatorEnum.Not, expression));
+	    }
+	    return this;
+	}
+
+    	/// <summary>
 	/// Returns true if nothing has been added to the clause; otherwise false.
 	/// </summary>
-	public virtual Boolean IsEmpty {
-	    get { return fragments.Count==0; }
+	public Boolean IsEmpty {
+	    get { return expressions.Count==0; }
 	}
 
-    	internal Boolean IsBetweenConjunction(FilterConjunction conjunction) {
-	    switch (conjunction) {
-		case FilterConjunction.AndBetween :
-		case FilterConjunction.OrBetween :
-		case FilterConjunction.AndNotBetween :
-		case FilterConjunction.OrNotBetween :
-		    return true;
-		default :
-		    return false;
-	    }
-    	}
-
-	internal Boolean IsInConjunction(FilterConjunction conjunction) {
-	    switch (conjunction) {
-		case FilterConjunction.AndIn :
-		case FilterConjunction.OrIn :
-		case FilterConjunction.AndNotIn :
-		case FilterConjunction.OrNotIn :
-		    return true;
-		default :
-		    return false;
-	    }
-	}
     }
 }
--- src/DAO/SqlFilter.cs
+++ src/DAO/SqlFilter.cs
@@ -10,22 +10,6 @@ namespace Spring2.Core.DAO {
     /// </summary>
     public class SqlFilter : DatabaseFilter, IDatabaseExpression {
     	
-    	private struct LogicalExpression {
-    	    public LogicalOperatorEnum Operator;
-    	    public IDatabaseExpression Expression;
-    		
-    	    public LogicalExpression(IDatabaseExpression expression) {
-    	    	Expression = expression;
-    		Operator = LogicalOperatorEnum.None;
-    	    }
-	    public LogicalExpression(LogicalOperatorEnum op, IDatabaseExpression expression) {
-		Expression = expression;
-		Operator = op;
-	    }
-	}
-
-    	private ArrayList expressions = new ArrayList();
-    	
 	public SqlFilter() {
 	}
     	
@@ -33,199 +17,24 @@ namespace Spring2.Core.DAO {
 	    expressions.Add(new LogicalExpression(expression));
 	}
 
+    	/// <summary>
+    	/// Returns the entire WHERE clause, including "WHERE", or blank if there are not any contained expressions
+    	/// </summary>
 	public override String Statement {
 	    get {
-		// TODO:  this should probably be done a better way, this will cause the building of the expression twice
-		if (Expression.Length == 0) {
+		String expression = Expression;
+		if (expression.Length == 0) {
 		    return String.Empty;
 		}
 
-		return " WHERE " + Expression;
+		return " WHERE " + expression;
 	    }
 	}
 	
-	#region old code
-	public override string InnerStatement {
-	    get { throw new NotImplementedException(); }
-	}
-
-	
-	//	public override String InnerStatement {
-	//	    get {
-	//		if (Fragments.Count == 0) {
-	//			return String.Empty;
-	//		}
-	//
-	//		StringBuilder sb = new StringBuilder();
-	//		foreach (FilterClause fragment in Fragments) {
-	//		    if (sb.Length > 0) {
-	//			switch (fragment.Conjunction) {
-	//			    case FilterConjunction.And :
-	//			    case FilterConjunction.AndEquals :
-	//			    case FilterConjunction.AndNotEquals :
-	//			    case FilterConjunction.AndBetween :
-	//			    case FilterConjunction.AndNotBetween :
-	//			    case FilterConjunction.AndIn :
-	//			    case FilterConjunction.AndNotIn :
-	//			    case FilterConjunction.AndLike :
-	//			    case FilterConjunction.AndNotLike :
-	//				sb.Append(" AND ");
-	//				break;
-	//			    case FilterConjunction.Or :
-	//			    case FilterConjunction.OrEquals :
-	//			    case FilterConjunction.OrNotEquals :
-	//			    case FilterConjunction.OrBetween :
-	//			    case FilterConjunction.OrNotBetween :
-	//			    case FilterConjunction.OrIn :
-	//			    case FilterConjunction.OrNotIn :
-	//			    case FilterConjunction.OrLike :
-	//			    case FilterConjunction.OrNotLike :
-	//				sb.Append(" OR ");
-	//				break;
-	//			    default :
-	//				throw new ArgumentException("Unexpected filter conjunction");
-	//			}
-	//		    }
-	//
-	//		    if (!(fragment.Value is DatabaseFilter || fragment.Key == null)) {
-	//			sb.Append(fragment.Key);
-	//		    
-	//			switch (fragment.Conjunction) {
-	//			    case FilterConjunction.AndEquals :
-	//			    case FilterConjunction.OrEquals :
-	//				if (fragment.Value != null) {
-	//				    sb.Append("=");
-	//				} else {
-	//				    sb.Append(" IS NULL");
-	//				}
-	//				break;
-	//			    case FilterConjunction.AndNotEquals :
-	//			    case FilterConjunction.OrNotEquals :
-	//				if (fragment.Value != null) {
-	//				    sb.Append("<>");
-	//				} else {
-	//				    sb.Append(" IS NOT NULL");
-	//				}
-	//				break;
-	//			    case FilterConjunction.AndBetween :
-	//			    case FilterConjunction.OrBetween :
-	//				sb.Append(" BETWEEN ");
-	//				break;
-	//			    case FilterConjunction.AndNotBetween :
-	//			    case FilterConjunction.OrNotBetween :
-	//				sb.Append(" NOT BETWEEN ");
-	//				break;
-	//			    case FilterConjunction.AndIn :
-	//			    case FilterConjunction.OrIn :
-	//				sb.Append(" IN ");
-	//				break;
-	//			    case FilterConjunction.AndNotIn :
-	//			    case FilterConjunction.OrNotIn :
-	//				sb.Append(" NOT IN ");
-	//				break;
-	//			    case FilterConjunction.And :
-	//			    case FilterConjunction.Or :
-	//				break;
-	//			    case FilterConjunction.AndLike :
-	//			    case FilterConjunction.OrLike :
-	//				sb.Append(" LIKE ");
-	//				break;
-	//			    case FilterConjunction.AndNotLike :
-	//			    case FilterConjunction.OrNotLike :
-	//				sb.Append(" NOT LIKE ");
-	//				break;
-	//			    default :
-	//				throw new ArgumentException("Unexpected filter conjunction");
-	//			}
-	//		    }
-	//
-	//		    if (IsBetweenConjunction(fragment.Conjunction)) {
-	//			sb.Append("@").Append(fragment.Key).Append("1 AND @").Append(fragment.Key).Append("2");		    	
-	//		    } else if (IsInConjunction(fragment.Conjunction)) {
-	//		    	if (fragment.Values.Length > 0) {
-	//		    	    sb.Append("(");
-	//		    	    for(Int32 i=0; i < fragment.Values.Length; i++) {
-	//		    	    	Object value = fragment.Values[i];
-	//		    	    	if (i > 0) {
-	//		    	    	    sb.Append(", ");
-	//		    	    	}
-	//		    	    	sb.Append("@").Append(fragment.Key).Append((i + 1).ToString());
-	//		    	    }
-	//		    	    sb.Append(")");
-	//		    	}
-	//		    } else if (fragment.Value is DatabaseFilter) {
-	//			sb.Append("(").Append(((DatabaseFilter)fragment.Value).InnerStatement).Append(")");
-	//		    } else if (fragment.Key == null) {
-	//			sb.Append("(").Append(fragment.Value).Append(")");
-	//		    } else {
-	//			if (fragment.Value != null) {
-	//			    sb.Append("@").Append(fragment.Key);
-	//			}
-	//		    }
-	//		}
-	//
-	//		return sb.ToString();
-	//	    }
-	//	}
-	//
-	//	public override IDataParameterCollection Parameters {
-	//	    get {
-	//	    	SqlCommand command = new SqlCommand();
-	//	    	AddParameters(command.Parameters, Fragments);
-	//	    	return command.Parameters;
-	//	    }
-	//	}
-	//    	
-	//    	private void AddParameters(SqlParameterCollection parameters, IList fragments) {
-	//	    foreach(FilterClause fragment in fragments) {
-	//		if (fragment.Value is DatabaseFilter) {
-	//		    AddParameters(parameters, ((DatabaseFilter)fragment.Value).Fragments);
-	//		} else if (fragment.Key == null || fragment.Key.Length == 0) {
-	//		    // must be a constant expression
-	//		} else {
-	//		    if (IsBetweenConjunction(fragment.Conjunction)) {
-	//			parameters.Add(new SqlParameter("@" + fragment.Key + "1", fragment.Values[0]));
-	//		    	parameters.Add(new SqlParameter("@" + fragment.Key + "2", fragment.Values[1]));
-	//		    } else if (IsInConjunction(fragment.Conjunction)) {
-	//			for(Int32 i=0; i < fragment.Values.Length; i++) {
-	//			    parameters.Add(new SqlParameter("@" + fragment.Key + (i+1).ToString(), fragment.Values[i]));
-	//			}
-	//		    } else if (fragment.Value != null) {
-	//		    	parameters.Add(new SqlParameter("@" + fragment.Key, fragment.Value));
-	//		    }
-	//		}
-	//	    }
-	//	}
-	#endregion
-
-	public SqlFilter And(IDatabaseExpression expression) {
-	    if (expressions.Count==0) {
-	    	expressions.Add(new LogicalExpression(expression));
-	    } else {
-		expressions.Add(new LogicalExpression(LogicalOperatorEnum.And, expression));
-	    }
-	    return this;
-	}
-
-	public SqlFilter Or(IDatabaseExpression expression) {
-	    if (expressions.Count==0) {
-		expressions.Add(new LogicalExpression(expression));
-	    } else {
-		expressions.Add(new LogicalExpression(LogicalOperatorEnum.Or, expression));
-	    }
-	    return this;
-	}
-
-	public SqlFilter Not(IDatabaseExpression expression) {
-	    if (expressions.Count==0) {
-		expressions.Add(new LogicalExpression(expression));
-	    } else {
-		expressions.Add(new LogicalExpression(LogicalOperatorEnum.Not, expression));
-	    }
-	    return this;
-	}
-	
-	public String Expression {
+	/// <summary>
+	/// This is the independent fragment based contained expressions.
+	/// </summary>
+	public override String Expression {
 	    get {
 	    	if (expressions.Count ==0) {
 	    	    return String.Empty;
@@ -261,6 +70,9 @@ namespace Spring2.Core.DAO {
 	    }
 	}
 
+	/// <summary>
+	/// Collection of data parameters to be added to the command when executed.
+	/// </summary>
 	public override IDataParameterCollection Parameters {
 	    get {
 		SqlParameterList parameters = new SqlParameterList();
@@ -269,13 +81,6 @@ namespace Spring2.Core.DAO {
 		}
 	    	return parameters;
 	    }
-	}
-
-	/// <summary>
-	/// Returns true if nothing has been added to the clause; otherwise false.
-	/// </summary>
-	public override Boolean IsEmpty {
-	    get { return expressions.Count==0; }
 	}
 
     }
