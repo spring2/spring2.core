702e21147b98a34856c2593f642e4345c27de791
cort <none@none>
2007-03-21 19:41:48 +0000

    add an object to help facilitate testing of controllers

---
+++ src/Maverick/MockHttpWorkerRequest.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Text;
+using System.Web;
+
+using Spring2.Core.Util;
+
+namespace Spring2.Core.Maverick {
+    /// <summary>
+    /// Summary description for MockHttpWorkerRequest.
+	/// For more ideas, see:
+	/// http://haacked.com/archive/2005/06/11/Simulating_HttpContext.aspx
+    /// </summary>
+    public class MockHttpWorkerRequest : HttpWorkerRequest {
+
+	private NameValueCollection form = new NameValueCollection();
+	private String queryString = String.Empty;
+	private NameValueCollection cookies = new NameValueCollection();
+    	private String rawUrl = String.Empty;
+	private StringDictionary headerValues = new StringDictionary();
+
+	public MockHttpWorkerRequest() {
+	    Initialize();
+	}
+
+	public MockHttpWorkerRequest(String rawUrl) {
+	    Initialize();
+	}
+	
+    	public MockHttpWorkerRequest(NameValueCollection form) {
+	    this.form = form;
+	    Initialize();
+	}
+
+	/// <summary>
+	/// Sets up the initial values that are to be found in the header
+	/// </summary>
+	private void Initialize() {
+	    headerValues.Add(HttpWorkerRequest.HeaderContentLength.ToString(), (Content.Length - 2).ToString());
+	    headerValues.Add(HttpWorkerRequest.HeaderContentType.ToString(), "application/x-www-form-urlencoded");
+	    headerValues.Add(HttpWorkerRequest.HeaderAcceptLanguage.ToString(), "en-US");
+	}
+
+	public void SetHeaderValue(Int32 index, String value) {
+	    headerValues[index.ToString()] = value;
+	}
+
+	public override string GetUriPath() {
+	    return "/localhost/";
+	}
+
+	public override string GetQueryString() {
+	    return queryString;
+	}
+
+	public override string GetRawUrl() {
+	    return rawUrl;
+	}
+
+	public override string GetHttpVerbName() {
+	    return "GET";
+	}
+
+	public override string GetHttpVersion() {
+	    return "HTTP/1.1";
+	}
+
+	public override string GetRemoteAddress() {
+	    return "remoteaddress";
+	}
+
+	public override int GetRemotePort() {
+	    throw new NotImplementedException();
+	}
+
+	public override string GetLocalAddress() {
+	    return "host";
+	}
+
+	public override int GetLocalPort() {
+	    return 80;
+	}
+
+	public override void SendStatus(int statusCode, string statusDescription) {
+	    throw new NotImplementedException();
+	}
+
+	public override void SendKnownResponseHeader(int index, string value) {
+	    throw new NotImplementedException();
+	}
+
+	public override void SendUnknownResponseHeader(string name, string value) {
+	    throw new NotImplementedException();
+	}
+
+	public override void SendResponseFromMemory(byte[] data, int length) {
+	    throw new NotImplementedException();
+	}
+
+	public override void SendResponseFromFile(string filename, long offset, long length) {
+	    throw new NotImplementedException();
+	}
+
+	public override void SendResponseFromFile(IntPtr handle, long offset, long length) {
+	    throw new NotImplementedException();
+	}
+
+	public override void FlushResponse(bool finalFlush) {
+	    throw new NotImplementedException();
+	}
+
+	public override void EndOfRequest() {
+	    throw new NotImplementedException();
+	}
+
+	public override string GetKnownRequestHeader(int index) {
+	    if (headerValues.ContainsKey(index.ToString())) {
+	    	return headerValues[index.ToString()];
+	    } else {
+		//System.Diagnostics.Debug.WriteLine("GetKnownRequestHeader not implemented for index: " + index);
+		return String.Empty;
+	    }
+	}
+
+	public override byte[] GetPreloadedEntityBody() {
+	    return Encoding.ASCII.GetBytes(StringUtil.RemoveTrailingNewLine(Content));
+	}
+
+	private String Content {
+	    get {
+		StringBuilder sb = new StringBuilder();
+		for(Int32 i=0; i<form.Keys.Count; i++) {
+		    String key = form.Keys[i];
+		    String value = form[key];
+		    sb.Append(key).Append("=").Append(value);
+		    if (i < form.Keys.Count-1) {
+			sb.Append("&");
+		    }
+		}
+		if (form.Keys.Count>0) {
+		    sb.Append("\r\n");
+		}
+
+		return sb.ToString();
+	    }
+	}
+    }
+
+}
