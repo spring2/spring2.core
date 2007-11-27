72f71d12a93400f2373fc0e34857fee3fa246fec
cort <none@none>
2007-11-27 16:10:23 +0000

    updated version number for unreleased builds

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -9,4 +9,4 @@
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: System.Reflection.AssemblyVersion("1.1.58.3874")]
+[assembly: System.Reflection.AssemblyVersion("1.1.60.0")]
