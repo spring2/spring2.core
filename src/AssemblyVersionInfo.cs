1eac14f41c55973418b06f79781470b5ab54464c
jeffm <none@none>
2007-12-30 00:10:04 +0000

    update to next version

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -9,4 +9,4 @@
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: System.Reflection.AssemblyVersion("1.1.65.3680")]
+[assembly: System.Reflection.AssemblyVersion("1.1.66.6180")]
