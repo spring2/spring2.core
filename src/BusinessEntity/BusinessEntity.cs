73e00de4697704165f52efc9f4d6a6c3a20e3e2e
bspiglejr <none@none>
2008-06-09 15:32:41 +0000

    Mark BusinessEntity serializable.

--- src/BusinessEntity/BusinessEntity.cs
+++ src/BusinessEntity/BusinessEntity.cs
@@ -4,6 +4,7 @@ using Spring2.Core.DAO;
 
 namespace Spring2.Core.BusinessEntity {
     
+	[Serializable]
     public abstract class BusinessEntity : Spring2.Core.DataObject.DataObject, IBusinessEntity {
         
         protected Boolean isNew = true;
