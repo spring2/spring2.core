6c68753abec1ad61513616074c2e8ad45222177d
ryanw <none@none>
2008-10-05 06:57:06 +0000

    Added some capability to force failures through a ConfigurationSetting value

--- src/Payment/Test/TestSplitPaymentProvider.cs
+++ src/Payment/Test/TestSplitPaymentProvider.cs
@@ -1,5 +1,6 @@
 using System;
 using Spring2.Core.Types;
+using Spring2.Core.Configuration;
 
 namespace Spring2.Core.Payment.Test {
 
@@ -9,51 +10,120 @@ namespace Spring2.Core.Payment.Test {
     public class TestSplitPaymentProvider : ISplitPaymentProvider {
 
 	public PaymentResult Authorize(CurrencyType amount, StringType address, StringType postalCode, StringType providerAccountNumber, StringType creditCardNumber, StringType cardExpirationDate, StringType cvv2, StringType invoiceNumber) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    CheckForSuccess(result);
+	    return result;
 	}
 
 	public PaymentResult Charge(StringType providerAccountNumber, CurrencyType amount, StringType address, StringType postalCode, StringType cardNumber, StringType cardExpirationDate, StringType cvv2, StringType invoiceNumber) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult ChargeWithSplit(StringType providerAccountNumber, CurrencyType totalAmount, CurrencyType commissionableAmount, StringType address, StringType postalCode, StringType cardNumber, StringType cardExpirationDate, StringType cvv2, StringType invoiceNumber, DecimalType splitFractionToMaster) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult Credit(StringType providerAccountNumber, CurrencyType amount, StringType invoiceNumber) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult Refund(StringType providerAccountNumber, StringType originalTransactionId, CurrencyType refundAmount, CurrencyType commissionableAmount, DecimalType originalFractionToSplit) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult Settle(StringType providerAccountNumber, StringType originalTransactionId) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult Void(StringType referenceNumber, StringType transactionId, CurrencyType amount) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
+	}
+
+	public PaymentResult Split(StringType sourceAccount, CurrencyType amount, StringType transactionNumber) {
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult Split(StringType sourceAccount, StringType recipientAccount, CurrencyType amount, StringType transactionNumber) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult VoidSplit(StringType recipientAccount, CurrencyType amount, StringType invoiceNumber) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
 	public PaymentResult PingAccount(StringType proPayAccountEmail, StringType ssn) {
-	    return GetResult();
+	    PaymentResult result = GetResult();
+	    if (result.ResultCode != "00") {
+		CheckForSuccess(result);
+	    }
+	    return result;
 	}
 
     	private PaymentResult GetResult() {
+	    IConfigurationProvider currentConfig = ConfigurationProvider.Instance;
     	    PaymentResult result = new PaymentResult();
-    	    result.ResultCode = "00";
-    	    result.ResultMessage = "Success";
+	    result.ResultCode = currentConfig.Settings["SplitPayment.Test.ResultCode"]; // "00";
+	    result.ResultMessage = currentConfig.Settings["SplitPayment.Test.ResultMesage"]; // "Success";
     	    return result;
     	}
+
+	private void CheckForSuccess(PaymentResult result) {
+	    if (result.ResultCode != "00") {
+		PaymentResult exceptionResult = new PaymentResult();
+		exceptionResult.AccountNumber = "TestAccountNumber";
+		exceptionResult.ApprovalNumber = "TestApprovalNumber";
+		exceptionResult.AuthorizationNumber = "TestAuthNumber";
+		exceptionResult.AVSResponseCode = "";
+		exceptionResult.CVVResponseCode = "";
+		exceptionResult.ReferenceNumber = "TestReference";
+		exceptionResult.ResponseCode = 22;
+		exceptionResult.ResultCode = "1";
+		exceptionResult.ResultMessage = "This is a test failure message.";
+		exceptionResult.TransactionAmount = new CurrencyType(100.00);
+		exceptionResult.TransactionDateTime = DateTime.Now;
+		exceptionResult.TransactionId = "444";
+		exceptionResult.ValidAddress = BooleanType.FALSE;
+		exceptionResult.ValidCvv = BooleanType.FALSE;
+		exceptionResult.ValidPostalCode = BooleanType.FALSE;
+		throw new PaymentFailureException(exceptionResult, "Failed to process the payment. {0}: {1}.", exceptionResult.ResultCode, "(ProPay) '" + exceptionResult.ResultMessage + "'");
+	    }
+	}
     	
     }
 }
