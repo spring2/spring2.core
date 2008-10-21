e40ac87408294375bed2356387ac443a4cd9880d
ryanw <none@none>
2008-10-21 15:18:05 +0000

    *** empty log message ***

---
+++ src/Payment/FundsUnavailableException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Spring2.Core.Types;
+
+namespace Spring2.Core.Payment {
+    public class FundsUnavailableException : Exception {
+	public FundsUnavailableException() : 
+	    base("Insufficient funds") {
+	}
+	public FundsUnavailableException(CurrencyType amountLacking) : 
+	    base("Insufficient funds (" + amountLacking + ")"){
+	}
+    }
+}
