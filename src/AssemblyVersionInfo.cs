7ea686f2ee21be3f0d3179bd91c37048d6d3e31e
cort <none@none>
2009-04-29 23:24:53 +0000

    update version file and getversion to expect the ccnet output format

--- src/AssemblyVersionInfo.cs
+++ src/AssemblyVersionInfo.cs
@@ -1,12 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
 //
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: System.Reflection.AssemblyVersion("1.1.97.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.97.0")]
