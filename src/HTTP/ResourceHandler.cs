144c586587792a98e79b3b6a88a98238c472528b
jeffm <none@none>
2009-05-06 15:57:02 +0000

    Add ability to choose to use file system to look up resources.

--- src/HTTP/ResourceHandler.cs
+++ src/HTTP/ResourceHandler.cs
@@ -10,22 +10,16 @@ namespace Spring2.Core.HTTP {
     public class ResourceHandler : IHttpHandler {
 	private String contentType = String.Empty;
 	private ImageFormat imageFormat = null;
-	private const String APPSETTING = "Spring2.Core.HTTP.ResourceHandler_ResourceDll";
+	private const String APPSETTING_RESOURCEDLL = "Spring2.Core.HTTP.ResourceHandler_ResourceDll";
+	private const String APPSETTING_USEFILESYSTEM = "Spring2.Core.HTTP.ResourceHandler_UseFileSystem";
 
 	public void ProcessRequest(HttpContext context) {
 	    String resource = context.Request.Params["d"];
 	    SetTypeAndImageFormat(resource);
-	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
-	    String resourceProperty = resource.Substring(resourceObject.Length + 1).Replace(".", "_");
-
-	    Assembly asm = Assembly.Load(ConfigurationSettings.AppSettings[APPSETTING]);
-	    System.Resources.ResourceManager rm = new System.Resources.ResourceManager(resourceObject, asm);
-
-	    if(imageFormat != null) {
-		Bitmap bitmap = (Bitmap)rm.GetObject(resourceProperty);
-		bitmap.Save(context.Response.OutputStream, imageFormat);
+	    if(ConfigurationSettings.AppSettings[APPSETTING_USEFILESYSTEM] == null || ConfigurationSettings.AppSettings[APPSETTING_USEFILESYSTEM].ToUpper() != "TRUE") {
+		this.ProcessResource(context, resource);
 	    } else {
-		context.Response.Write(rm.GetString(resourceProperty));
+		this.ProcessResourceFromFile(context, resource);
 	    }
 	    context.Response.ContentType = contentType;
 	    context.Response.End();
@@ -34,6 +28,35 @@ namespace Spring2.Core.HTTP {
 	public bool IsReusable {
 	    get {
 		return false;
+	    }
+	}
+
+	private void ProcessResource(HttpContext context, String resource) {
+	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
+	    String resourceProperty = resource.Substring(resourceObject.Length + 1).Replace(".", "_").Replace("/", @"\");
+
+	    Assembly asm = Assembly.Load(ConfigurationSettings.AppSettings[APPSETTING_RESOURCEDLL]);
+	    System.Resources.ResourceManager rm = new System.Resources.ResourceManager(resourceObject, asm);
+
+	    if(imageFormat != null) {
+		Bitmap bitmap = (Bitmap)rm.GetObject(resourceProperty);
+		bitmap.Save(context.Response.OutputStream, imageFormat);
+	    } else {
+		context.Response.Write(rm.GetString(resourceProperty));
+	    }
+	}
+
+	private void ProcessResourceFromFile(HttpContext context, String resource) {
+	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
+	    String resourceProperty = resource.Substring(resourceObject.Length + 1);
+
+	    String fileName = ConfigurationSettings.AppSettings[resourceObject] + @"\" + resourceProperty;
+
+	    if(imageFormat != null) {
+		Image img = Image.FromFile(fileName);
+		img.Save(context.Response.OutputStream, imageFormat);
+	    } else {
+		context.Response.Write(System.IO.File.OpenText(fileName).ReadToEnd());
 	    }
 	}
 
