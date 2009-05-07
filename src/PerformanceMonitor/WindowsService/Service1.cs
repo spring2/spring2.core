482d6a7d64f20720017ca62201bee8eb0b0bb2bd
bspiglejr <none@none>
2009-05-07 15:48:12 +0000

    Change windows service to target 2.0 framework instead of 3.5.

--- src/PerformanceMonitor/WindowsService/Service1.cs
+++ src/PerformanceMonitor/WindowsService/Service1.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading;
