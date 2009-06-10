f9451ab1feda5f97d45a4da8f468e51c91cd174a
rolandk <none@none>
2009-06-10 17:02:48 +0000

    Removed the code to determine the location of the resource into a utility, so it may be used else where that uses the resources.

--- src/HTTP/ResourceHandler.cs
+++ src/HTTP/ResourceHandler.cs
@@ -12,26 +12,13 @@ namespace Spring2.Core.HTTP {
     public class ResourceHandler : IHttpHandler {
 	private String contentType = String.Empty;
 	private ImageFormat imageFormat = null;
-	private const String APPSETTING_RESOURCEDLL = "Spring2.Core.HTTP.ResourceHandler_ResourceDll";
-	private const String APPSETTING_RESOURCETYPE = "Spring2.Core.HTTP.ResourceHandler_Type";
 
 	public void ProcessRequest(HttpContext context) {
 	    String resource = context.Request.Params["d"];
 	    SetTypeAndImageFormat(resource);
 
-	    String value = ConfigurationSettings.AppSettings[APPSETTING_RESOURCETYPE] != null ? ConfigurationSettings.AppSettings[APPSETTING_RESOURCETYPE].ToUpper() : String.Empty;
-	    switch(value) {
-		case "FILESYSTEM":
-		    this.ProcessResourceFromFile(context, resource);
-		    break;
-		case "RESOURCEFILE":
-		    this.ProcessResourcesFromResourceFile(context, resource);
-		    break;
-		case "RESOURCEASSEMBLY":
-		    this.ProcessResourceFromAssembly(context, resource);
-		    break;
-		default:
-		    throw new FormatException("'" + APPSETTING_RESOURCETYPE + "' app setting needs to be set to 'FileSystem', 'ResourceFile', or 'ResourceAssembly'");
+	    using (MemoryStream stream = ResourceUtility.GetResourceStream(context, resource, imageFormat)) {
+		stream.WriteTo(context.Response.OutputStream);		
 	    }
 
 	    context.Response.ContentType = contentType;
@@ -41,51 +28,6 @@ namespace Spring2.Core.HTTP {
 	public bool IsReusable {
 	    get {
 		return false;
-	    }
-	}
-
-	private void ProcessResourceFromAssembly(HttpContext context, String resource) {
-	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
-	    String resourceProperty = resource.Substring(resourceObject.Length + 1).Replace(".", "_").Replace("/", @"\");
-
-	    Assembly asm = Assembly.Load(ConfigurationSettings.AppSettings[APPSETTING_RESOURCEDLL]);
-	    ResourceManager rm = new ResourceManager(resourceObject, asm);
-
-	    if(imageFormat != null) {
-		Bitmap bitmap = (Bitmap)rm.GetObject(resourceProperty);
-		ImageToOutputStream(bitmap as Image, context);
-	    } else {
-		context.Response.Write(rm.GetString(resourceProperty));
-	    }
-	}
-
-	private void ProcessResourcesFromResourceFile(HttpContext context, String resource) {
-	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
-	    String resourceProperty = resource.Substring(resourceObject.Length + 1).Replace(".", "_").Replace("/", @"\");
-
-	    ResourceManager rm = ResourceManager.CreateFileBasedResourceManager(resourceObject, context.Server.MapPath("~/Bin").Replace(@"\~", ""), null);
-
-	    if(imageFormat != null) {
-		Bitmap bitmap = (Bitmap)rm.GetObject(resourceProperty);
-		ImageToOutputStream(bitmap as Image, context);
-	    } else {
-		context.Response.Write(rm.GetString(resourceProperty));
-	    }
-	}
-
-	private void ProcessResourceFromFile(HttpContext context, String resource) {
-	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
-	    String resourceProperty = resource.Substring(resourceObject.Length + 1);
-
-	    String fileName = ConfigurationSettings.AppSettings[resourceObject] + @"\" + resourceProperty;
-
-	    if(imageFormat != null) {
-		Image img = Image.FromFile(fileName);
-		ImageToOutputStream(img, context);
-	    } else {
-		StreamReader sr = File.OpenText(fileName);
-		context.Response.Write(sr.ReadToEnd());
-		sr.Close();
 	    }
 	}
 
---
+++ src/HTTP/ResourceUtility.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Configuration;
+using System.Web;
+using System.Resources;
+using System.Reflection;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Spring2.Core.HTTP {
+    public class ResourceUtility {
+	private const String APPSETTING_RESOURCEDLL = "Spring2.Core.HTTP.ResourceHandler_ResourceDll";
+	private const String APPSETTING_RESOURCETYPE = "Spring2.Core.HTTP.ResourceHandler_Type";
+
+	/// <summary>
+	/// Gets the resource stream. Assumes that this resource is a text based resource file.
+	/// </summary>
+	/// <param name="context">The context.</param>
+	/// <param name="resource">The resource.</param>
+	/// <returns></returns>
+	public static MemoryStream GetResourceStream(HttpContext context, string resource) {
+	    return GetResourceStream(context, resource, null);
+	}
+
+	/// <summary>
+	/// Gets the resource stream. If desiredFormat is null, it is assumed that the resouce is text. Otherwise, it is an image.
+	/// </summary>
+	/// <param name="context">The context.</param>
+	/// <param name="resource">The resource.</param>
+	/// <param name="desiredFormat">The desired format.</param>
+	/// <returns></returns>
+	public static MemoryStream GetResourceStream(HttpContext context, string resource, ImageFormat desiredFormat) {
+	    MemoryStream stream = null;
+
+	    String value = ConfigurationSettings.AppSettings[APPSETTING_RESOURCETYPE] ?? String.Empty;
+	    switch (value.ToUpper()) {
+		case "FILESYSTEM":
+		    stream = ProcessResourceFromFile(context, resource, desiredFormat);
+		    break;
+		case "RESOURCEFILE":
+		    stream = ProcessResourcesFromResourceFile(context, resource, desiredFormat);
+		    break;
+		case "RESOURCEASSEMBLY":
+		    stream = ProcessResourceFromAssembly(context, resource, desiredFormat);
+		    break;
+		default:
+		    throw new FormatException("'" + APPSETTING_RESOURCETYPE + "' app setting needs to be set to 'FileSystem', 'ResourceFile', or 'ResourceAssembly'");
+	    }
+
+	    return stream;
+	}
+
+	private static MemoryStream ProcessResourceFromFile(HttpContext context, String resource, ImageFormat desiredFormat) {
+	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
+	    String resourceProperty = resource.Substring(resourceObject.Length + 1);
+	    String fileName = ConfigurationSettings.AppSettings[resourceObject] + @"\" + resourceProperty;
+
+	    MemoryStream ms = new MemoryStream(File.ReadAllBytes(fileName));
+	    return ms;
+	}
+
+	private static MemoryStream ProcessResourceFromAssembly(HttpContext context, String resource, ImageFormat desiredFormat) {
+	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
+	    String resourceProperty = resource.Substring(resourceObject.Length + 1).Replace(".", "_").Replace("/", @"\");
+
+	    Assembly asm = Assembly.Load(ConfigurationSettings.AppSettings[APPSETTING_RESOURCEDLL]);
+	    ResourceManager rm = new ResourceManager(resourceObject, asm);
+
+	    return GetMemoryStreamFromObject(rm.GetObject(resourceProperty), desiredFormat);
+	}
+
+	private static MemoryStream ProcessResourcesFromResourceFile(HttpContext context, String resource, ImageFormat desiredFormat) {
+	    String resourceObject = resource.Substring(0, resource.IndexOf("."));
+	    String resourceProperty = resource.Substring(resourceObject.Length + 1).Replace(".", "_").Replace("/", @"\");
+	    ResourceManager rm = ResourceManager.CreateFileBasedResourceManager(resourceObject, context.Server.MapPath("~/Bin").Replace(@"\~", ""), null);
+
+	    return GetMemoryStreamFromObject(rm.GetObject(resourceProperty), desiredFormat);
+	}
+
+	private static MemoryStream GetMemoryStreamFromObject(object o, ImageFormat desiredFormat) {
+	    MemoryStream stream = new MemoryStream();
+	    if (desiredFormat == null) { // We only expect image files and text files.
+		UTF8Encoding enc = new UTF8Encoding();
+		byte[] bytes = enc.GetBytes(o as string);
+		stream.Write(bytes, 0, bytes.Length);
+	    } else {
+		using (Bitmap image = o as Bitmap) {
+		    image.Save(stream, desiredFormat);
+		}
+	    }
+	    return stream;
+	}
+    }
+}
