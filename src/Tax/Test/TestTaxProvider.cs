731133b63abfd470e4b577c5d29c6eb059515b25
cort <none@none>
2010-01-11 18:52:35 +0000

    updated to always calculate the tax jurisdictions like a live provider would. fixed calculation for jurisdiction amounts so that the sum will match the total tax

--- src/Tax/Test/TestTaxProvider.cs
+++ src/Tax/Test/TestTaxProvider.cs
@@ -14,12 +14,11 @@ namespace Spring2.Core.Tax.Test {
 	}
 
 	public TaxRateInfo GetTaxRateForArea(IdType id, DateType dateType) {
-		if(!id.IsValid)
-		{
-			MessageList errors = new MessageList();
-			errors.Add(new InvalidTaxAreaIdException());
-			throw new MessageListException(errors);
-		}
+	    if (!id.IsValid) {
+		MessageList errors = new MessageList();
+		errors.Add(new InvalidTaxAreaIdException());
+		throw new MessageListException(errors);
+	    }
 
 	    TaxRateInfo taxRateInfo = new TaxRateInfo();
 	    taxRateInfo.CityTaxRate = 1.0M;
@@ -37,23 +36,25 @@ namespace Spring2.Core.Tax.Test {
 
 	public TaxResult Commit(TaxOrder order, StringType currencyCode) {
 	    CurrencyType total = CurrencyType.ZERO;
-	    foreach(TaxOrderLine line in order.Lines) {
+	    foreach (TaxOrderLine line in order.Lines) {
 		total += line.ExtendedPrice;
 	    }
 
 	    TaxResult result = new TaxResult();
-	    result.TotalTax = CurrencyType.Round(total * 0.066M,2);
+	    result.TotalTax = CurrencyType.Round(total * 0.066M, 2);
+	    result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions("CITY", "COUNTY", "REGION", result.TotalTax));
 	    return result;
 	}
 
 	public TaxResult GetQuoteTaxTotal(TaxOrder order, StringType currencyCode) {
 	    CurrencyType total = CurrencyType.ZERO;
-	    foreach(TaxOrderLine line in order.Lines) {
+	    foreach (TaxOrderLine line in order.Lines) {
 		total += line.ExtendedPrice;
 	    }
 
 	    TaxResult result = new TaxResult();
-	    result.TotalTax = CurrencyType.Round(total * 0.066M,2);
+	    result.TotalTax = CurrencyType.Round(total * 0.066M, 2);
+	    result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions("CITY", "COUNTY", "REGION", result.TotalTax));
 	    return result;
 	}
 
@@ -70,6 +71,9 @@ namespace Spring2.Core.Tax.Test {
 	    taxArea.TotalTaxRate = 6.6M;
 	    taxArea.IsShippingTaxable = BooleanType.TRUE;
 	    taxArea.AddressValidated = BooleanType.TRUE;
+
+	    taxArea.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, 0));
+
 	    return taxArea;
 	}
 
@@ -81,7 +85,7 @@ namespace Spring2.Core.Tax.Test {
 
 	public TaxResult Commit(StringType street, StringType city, StringType county, StringType region, StringType postalCode, StringType country, DateType date, TaxOrder order) {
 	    CurrencyType total = CurrencyType.ZERO;
-	    foreach(TaxOrderLine line in order.Lines) {
+	    foreach (TaxOrderLine line in order.Lines) {
 		total += line.ExtendedPrice;
 	    }
 
@@ -94,17 +98,17 @@ namespace Spring2.Core.Tax.Test {
 	    result.Region = region;
 	    result.TaxAreaID = new IdType(1);
 	    result.TotalTaxRate = 6.6M;
-	    result.TotalTax = CurrencyType.Round(total * 0.066M,2);
+	    result.TotalTax = CurrencyType.Round(total * 0.066M, 2);
 
-		result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, total));
-	    
+	    result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, total));
+
 	    return result;
 	}
 
 	public TaxResult Calculate(StringType street, StringType city, StringType county, StringType region, StringType postalCode, StringType country, DateType date, TaxOrder order) {
 	    CurrencyType total = CurrencyType.ZERO;
-	    foreach(TaxOrderLine line in order.Lines) {
-		total += line.Price * line.Quantity.ToInt32() - line.DiscountAmount;
+	    foreach (TaxOrderLine line in order.Lines) {
+		total += line.Price * line.Quantity.ToInt32() - (line.DiscountAmount.IsValid ? line.DiscountAmount : 0);
 	    }
 
 	    TaxResult result = new TaxResult();
@@ -116,17 +120,17 @@ namespace Spring2.Core.Tax.Test {
 	    result.Region = region;
 	    result.TaxAreaID = new IdType(1);
 	    result.TotalTaxRate = 6.6M;
-	    result.TotalTax = CurrencyType.Round(total * 0.066M,2);
+	    result.TotalTax = CurrencyType.Round(total * 0.066M, 2);
 	    result.AddressValidated = BooleanType.TRUE;
-	    
-		result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, total));
-		
+
+	    result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, total));
+
 	    return result;
 	}
 
 	public TaxResult Calculate(StringType transactionId, StringType street, StringType city, StringType county, StringType region, StringType postalCode, StringType country, DateType date, TaxOrder order) {
 	    CurrencyType total = CurrencyType.ZERO;
-	    foreach(TaxOrderLine line in order.Lines) {
+	    foreach (TaxOrderLine line in order.Lines) {
 		total += line.Price * line.Quantity.ToInt32() - line.DiscountAmount;
 	    }
 
@@ -139,46 +143,45 @@ namespace Spring2.Core.Tax.Test {
 	    result.Region = region;
 	    result.TaxAreaID = new IdType(1);
 	    result.TotalTaxRate = 6.6M;
-	    result.TotalTax = CurrencyType.Round(total * 0.066M,2);
+	    result.TotalTax = CurrencyType.Round(total * 0.066M, 2);
 	    result.AddressValidated = BooleanType.TRUE;
 
-		result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, total));
-	    
+	    result.TaxJurisdictions.AddRange(GetTestTaxJurisdictions(city, county, region, total));
+
 	    return result;
 	}
 
-	private System.Collections.ArrayList GetTestTaxJurisdictions(StringType county, StringType city, StringType region, CurrencyType total)
-	{
-		System.Collections.ArrayList results = new System.Collections.ArrayList();
+	private System.Collections.ArrayList GetTestTaxJurisdictions(StringType county, StringType city, StringType region, CurrencyType total) {
+	    System.Collections.ArrayList results = new System.Collections.ArrayList();
 
-		DecimalType countyRate = new DecimalType(1.5);
-		DecimalType cityRate = new DecimalType(.35);
-		DecimalType stateRate = new DecimalType(4.75);
+	    DecimalType countyRate = new DecimalType(1.5);
+	    DecimalType cityRate = new DecimalType(.35);
+	    DecimalType stateRate = new DecimalType(4.75);
 
-		TaxJurisdiction tj = null;
+	    TaxJurisdiction tj = null;
 
-		tj = new TaxJurisdiction();
-		tj.Amount = total * countyRate;
-		tj.Description = county;
-		tj.JurisdictionType = TaxJurisdictionTypeEnum.COUNTY;
-		tj.Rate = countyRate;
-		results.Add(tj);
+	    tj = new TaxJurisdiction();
+	    tj.Amount = total * countyRate/100.0;
+	    tj.Description = county;
+	    tj.JurisdictionType = TaxJurisdictionTypeEnum.COUNTY;
+	    tj.Rate = countyRate;
+	    results.Add(tj);
 
-		tj = new TaxJurisdiction();
-		tj.Amount = total * cityRate;
-		tj.Description = city;
-		tj.JurisdictionType = TaxJurisdictionTypeEnum.CITY;
-		tj.Rate = cityRate;
-		results.Add(tj);
+	    tj = new TaxJurisdiction();
+	    tj.Amount = total * cityRate / 100.0;
+	    tj.Description = city;
+	    tj.JurisdictionType = TaxJurisdictionTypeEnum.CITY;
+	    tj.Rate = cityRate;
+	    results.Add(tj);
 
-		tj = new TaxJurisdiction();
-		tj.Amount = total * stateRate;
-		tj.Description = region;
-		tj.JurisdictionType = TaxJurisdictionTypeEnum.STATE;
-		tj.Rate = stateRate;
-		results.Add(tj);
+	    tj = new TaxJurisdiction();
+	    tj.Amount = total * stateRate / 100.0;
+	    tj.Description = region;
+	    tj.JurisdictionType = TaxJurisdictionTypeEnum.STATE;
+	    tj.Rate = stateRate;
+	    results.Add(tj);
 
-		return results;
+	    return results;
 	}
 
     }
--- src/Tax/Test/TestTaxProviderTest.cs
+++ src/Tax/Test/TestTaxProviderTest.cs
@@ -4,32 +4,77 @@ using Spring2.Core.Types;
 using Spring2.Core.Tax;
 using Spring2.Core.Message;
 
-namespace Spring2.Core.Tax.Test
-{
-	[TestFixture()]
-	public class TestTaxProviderTest
-	{
-		[SetUp()]
-		public void Setup() 
-		{
-		}
+namespace Spring2.Core.Tax.Test {
+    [TestFixture()]
+    public class TestTaxProviderTest {
+	[SetUp()]
+	public void Setup() {
+	}
 
-		[Test()]
-		public void TaxAreaLookup1() 
-		{
-			try
-			{
-				TestTaxProvider tax = new TestTaxProvider("US");
+	[Test()]
+	public void TaxAreaLookup1() {
+	    try {
+		TestTaxProvider tax = new TestTaxProvider("US");
 
-				TaxRateInfo info = tax.GetTaxRateForArea(IdType.DEFAULT, DateType.DEFAULT);
-				Assert.Fail();	//	Should fail in the previous call
-			}
-			catch(MessageListException exc)
-			{
-				Assert.IsTrue(exc.Messages.Count > 0);
-				Assert.AreEqual(typeof(InvalidTaxAreaIdException), exc.Messages[0].GetType());
-			}
-		}
+		TaxRateInfo info = tax.GetTaxRateForArea(IdType.DEFAULT, DateType.DEFAULT);
+		Assert.Fail();	//	Should fail in the previous call
+	    } catch (MessageListException exc) {
+		Assert.IsTrue(exc.Messages.Count > 0);
+		Assert.AreEqual(typeof(InvalidTaxAreaIdException), exc.Messages[0].GetType());
+	    }
+	}
+
+	[Test]
+	public void TaxJurisdictionBreakdownShouldMatchTotal() {
+	    TestTaxProvider provider = new TestTaxProvider("US");
+	    TaxAreaList taxAreaList = provider.LookupTaxArea("6862 S Algonguian Ct", "Aurora", "", "CO", "80016", "USA", DateType.Now, BooleanType.FALSE);
+
+	    CurrencyType total = CurrencyType.ZERO;
+	    DecimalType rate = DecimalType.ZERO;
+	    foreach (TaxJurisdiction j in taxAreaList[0].TaxJurisdictions) {
+		total += j.Amount;
+		rate += j.Rate;
+	    }
+
+	    Assert.AreEqual(rate, taxAreaList[0].TotalTaxRate);
 
 	}
+
+	[Test()]
+	public void GetQuoteTaxTotal() {
+	    TaxOrder order = new TaxOrder();
+	    order.TaxAreaId = IdType.Parse("84070");
+
+	    TaxOrderLine taxOrderLine;
+
+	    taxOrderLine = new TaxOrderLine();
+	    taxOrderLine.Quantity = 1;
+	    taxOrderLine.Price = 500;
+	    taxOrderLine.ItemNumber = "001";
+	    order.Lines.Add(taxOrderLine);
+
+	    taxOrderLine = new TaxOrderLine();
+	    taxOrderLine.Quantity = 1;
+	    taxOrderLine.Price = 500;
+	    taxOrderLine.ItemNumber = "002";
+	    order.Lines.Add(taxOrderLine);
+
+
+	    TestTaxProvider provider = new TestTaxProvider("US");
+	    TaxResult result = provider.Calculate("10150 South Centennial Parkway", "Sandy", "", "UT", "84070", "USA", DateType.Now, order);
+
+	    Assert.AreEqual(new CurrencyType(66.00), result.TotalTax);
+
+	    CurrencyType total = CurrencyType.ZERO;
+	    DecimalType rate = DecimalType.ZERO;
+	    foreach (TaxJurisdiction j in result.TaxJurisdictions) {
+		total += j.Amount;
+		rate += j.Rate;
+	    }
+
+	    Assert.AreEqual(rate, result.TotalTaxRate);
+	    Assert.AreEqual(result.TotalTax, total);
+	}
+
+    }
 }
