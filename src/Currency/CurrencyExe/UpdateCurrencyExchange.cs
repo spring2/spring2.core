945ce9233aef6a7aa3f957a1634540e2643efae2
jeffd <none@none>
2010-01-21 20:48:53 +0000

    Renaming Program.cs to UpdateCurrencyExchange.cs

--- src/Currency/CurrencyExe/UpdateCurrencyExchange.cs
+++ src/Currency/CurrencyExe/UpdateCurrencyExchange.cs
@@ -7,14 +7,16 @@ using Spring2.Core.Types;
 using Spring2.Core.Currency.BusinessLogic;
 using Spring2.Core.Currency.DataObject;
 
-namespace Spring2.Core.Currency.CurrencyExe {
-    class Program {
+namespace Spring2.Core.Currency.UpdateCurrencyExchange {
+    class UpdateCurrencyExchange {
 	static void Main(string[] args) {
 	    // For now assume USD -> CAD
 	    try {
 		StringType currency = "CAD";
+		if (args.Length > 0) {
+		}
 		ICurrencyExchange line = CurrencyExchange.CheckForRateAndUpdate(currency);
-		Console.Out.Write(string.Format("Exchange rate for CAD (Canadian Dollar) based on USD (US Dollar) is {0} at {1}.", line.Rate, line.EffectiveDate));
+		Console.Out.Write(string.Format("Exchange rate for CAD (Canadian Dollar) based on USD (US Dollar) is {0} on {1}.", line.Rate, line.EffectiveDate));
 	    } catch (Exception ex) {
 		Console.Out.Write(String.Format("An error occurred checking for a new exchange rate.\nThe exception message is: {0}", ex.Message));
 	    }
