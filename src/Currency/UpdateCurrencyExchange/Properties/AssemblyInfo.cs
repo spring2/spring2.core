a55905cc7e65a640cf8e93c10669bb11a55ce053
jeffd <none@none>
2010-01-21 21:48:03 +0000

    Updated to reflect changes to the assembly name

--- src/Currency/UpdateCurrencyExchange/Properties/AssemblyInfo.cs
+++ src/Currency/UpdateCurrencyExchange/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Spring2.Core.Currency.CurrencyExe")]
+[assembly: AssemblyTitle("Spring2.Core.Currency.UpdateCurrencyExchange")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Spring2.Core.Currency.CurrencyExe")]
+[assembly: AssemblyProduct("Spring2.Core.Currency.UpdateCurrencyExchange")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
