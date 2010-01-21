07ba7c35c851fccad5aff07e553bf9447fc3f7c2
jeffd <none@none>
2010-01-21 21:09:00 +0000

    Added back log4net logging that was lost when files were renamed

--- src/Currency/UpdateCurrencyExchange/UpdateCurrencyExchange.cs
+++ src/Currency/UpdateCurrencyExchange/UpdateCurrencyExchange.cs
@@ -9,16 +9,19 @@ using Spring2.Core.Currency.DataObject;
 
 namespace Spring2.Core.Currency.UpdateCurrencyExchange {
     class UpdateCurrencyExchange {
+	private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
 	static void Main(string[] args) {
-	    // For now assume USD -> CAD
+	    log4net.Config.XmlConfigurator.Configure();
 	    try {
+		// For now assume USD -> CAD
 		StringType currency = "CAD";
 		if (args.Length > 0) {
 		}
 		ICurrencyExchange line = CurrencyExchange.CheckForRateAndUpdate(currency);
-		Console.Out.Write(string.Format("Exchange rate for CAD (Canadian Dollar) based on USD (US Dollar) is {0} on {1}.", line.Rate, line.EffectiveDate));
+		log.Info(string.Format("Exchange rate for CAD (Canadian Dollar) based on USD (US Dollar) is {0} on {1}.", line.Rate, line.EffectiveDate));
 	    } catch (Exception ex) {
-		Console.Out.Write(String.Format("An error occurred checking for a new exchange rate.\nThe exception message is: {0}", ex.Message));
+		log.Error(String.Format("An error occurred checking for a new exchange rate.\nThe exception message is: {0}", ex.Message));
 	    }
 	}
     }
