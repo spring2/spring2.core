878889607bbf62f1b1cccf6be221519f15f71c7a
timz <none@none>
2010-02-17 00:17:40 +0000

    renaming

--- src/CommunicationSubscription/sql/CommunicationSubscription.mk
+++
@@ -1,4 +0,0 @@
-sql/table/CommunicationSubscriptionType.table.log: 
-sql/view/vwCommunicationSubscriptionType.view.log: 
-sql/table/CommunicationSubscriptionTracking.table.log:  sql/table/CommunicationSubscriptionType.table.log
-sql/view/vwCommunicationSubscriptionTracking.view.log:  sql/view/vwCommunicationSubscriptionType.view.log
