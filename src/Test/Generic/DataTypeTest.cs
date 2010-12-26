f9dd8f8ae4dfa9213bb00fcf8de5232e8a0856cf
cort <none@none>
2010-12-26 05:45:33 +0000

    add open generic type that could be a replacement for Spring2.Core data types -- i.e. DataType<Int32>

---
+++ src/Test/Generic/DataTypeTest.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Spring2.Core.Types.Generic;
+using NUnit.Framework;
+using Spring2.Core.Types;
+
+namespace Spring2.Core.Test.Generic {
+
+    [TestFixture]
+    public class DataTypeTest {
+
+	[Test]
+	public void ShouldCompareEqual() {
+	    DataType<Int32> t1 = new DataType<Int32>(1);
+	    DataType<Int32> t2 = new DataType<Int32>(1);
+
+	    Assert.AreEqual(t1, t2);
+	    Assert.IsTrue(t1 == t2);
+	}
+
+	[Test]
+	public void ShouldCompareNotEqual() {
+	    DataType<Int32> t1 = new DataType<Int32>(1);
+	    DataType<Int32> t2 = new DataType<Int32>(2);
+
+	    Assert.AreNotEqual(t1, t2);
+	    Assert.IsTrue(t1 != t2);
+	}
+
+
+	[Test]
+	public void ShouldCompareLessThan() {
+	    DataType<Int32> t1 = new DataType<Int32>(1);
+	    Int32 t2 = 2;
+
+	    Assert.IsTrue(t1 < t2);
+	}
+
+	[Test]
+	public void ShouldCompareGreaterThan() {
+	    DataType<Int32> t1 = new DataType<Int32>(1);
+	    Int32 t2 = 2;
+
+	    Assert.IsTrue(t2 > t1);
+	}
+
+	[Test]
+	public void foo() {
+	    DataType<Int32> t1 = new DataType<Int32>(1);
+	    DataType<Int32> t2 = new DataType<Int32>(2);
+	    DataType<Int32> t1_1 = new DataType<Int32>(1);
+	    DataType<Int32> _unset = new DataType<Int32>(TypeState.UNSET);
+	    DataType<Int32> _default = new DataType<Int32>(TypeState.DEFAULT);
+
+
+	    Int32 i = t1.Value;
+	    Assert.AreEqual(1, i);
+	    try {
+		var foo = _unset.Value;
+	    } catch(InvalidOperationException) {
+		// pass
+	    }
+	    try {
+		var foo = _default.Value;
+	    } catch (InvalidOperationException) {
+		// pass
+	    }
+
+	    Assert.IsTrue(_unset.IsUnset);
+	    Assert.IsFalse(_unset.IsDefault);
+	    Assert.IsFalse(_unset.IsValid);
+
+	    Assert.IsFalse(_default.IsUnset);
+	    Assert.IsTrue(_default.IsDefault);
+	    Assert.IsFalse(_default.IsValid);
+
+	    Assert.IsFalse(t1.IsUnset);
+	    Assert.IsFalse(t1.IsDefault);
+	    Assert.IsTrue(t1.IsValid);
+
+	    Assert.IsTrue(t1 < t2);
+	    Assert.IsTrue(t2 > t1);
+	    Assert.IsTrue(t1 != t2);
+	    Assert.IsTrue(t1 == t1_1);
+
+	    Assert.IsTrue(t1 == 1);
+	    Assert.IsTrue(t1 != 2);
+
+	    Assert.IsTrue(IntegerType.DEFAULT > IntegerType.UNSET);
+	    Assert.IsTrue(IntegerType.ZERO > IntegerType.UNSET);
+	    Assert.IsTrue(IntegerType.ZERO > IntegerType.DEFAULT);
+
+	    Assert.IsFalse(IntegerType.DEFAULT < IntegerType.UNSET);
+	    Assert.IsFalse(IntegerType.ZERO < IntegerType.UNSET);
+	    Assert.IsFalse(IntegerType.ZERO < IntegerType.DEFAULT);
+
+	    Assert.IsTrue(IntegerType.UNSET < IntegerType.ZERO);
+	    Assert.IsTrue(IntegerType.DEFAULT < IntegerType.ZERO);
+	    Assert.IsTrue(IntegerType.UNSET < IntegerType.DEFAULT);
+
+	    Assert.IsFalse(IntegerType.UNSET > IntegerType.ZERO);
+	    Assert.IsFalse(IntegerType.DEFAULT > IntegerType.ZERO);
+	    Assert.IsFalse(IntegerType.UNSET > IntegerType.DEFAULT);
+
+	    Assert.IsFalse(_unset.Equals(null));
+	    Assert.IsFalse(_unset.Equals(_default));
+	    Assert.IsFalse(_default.Equals(_unset));
+	    Assert.IsFalse(_unset.Equals(t1));
+
+	    Assert.IsTrue(t1.Equals(t1_1));
+	    Assert.IsTrue(_unset.Equals(new DataType<Int32>(TypeState.UNSET)));
+	    Assert.IsTrue(_default.Equals(new DataType<Int32>(TypeState.DEFAULT)));
+
+	    Assert.IsFalse(t1.Equals(null));
+	    Assert.IsFalse(t1.Equals(_default));
+	    Assert.IsFalse(t1.Equals(_unset));
+
+
+	    Assert.IsFalse(t1.Equals(null));
+	    Assert.IsFalse(t1.Equals(new DataType<Int64>(1)));
+
+
+	    Assert.IsTrue(_default > _unset);
+	    Assert.IsTrue(t1 > _unset);
+	    Assert.IsTrue(t1 > _default);
+
+	    Assert.IsFalse(_default < _unset);
+	    Assert.IsFalse(t1 < _unset);
+	    Assert.IsFalse(t1 < _default);
+
+	    Assert.IsTrue(_unset < t1);
+	    Assert.IsTrue(_default < t1);
+	    Assert.IsTrue(_unset < _default);
+
+	    Assert.IsFalse(_unset > t1);
+	    Assert.IsFalse(_default > t1);
+	    Assert.IsFalse(_unset > _default);
+
+
+
+	    Assert.IsTrue(1 > _unset);
+	    Assert.IsTrue(1 > _default);
+
+	    Assert.IsFalse(1 < _unset);
+	    Assert.IsFalse(1 < _default);
+
+	    Assert.IsTrue(_unset < 1);
+	    Assert.IsTrue(_default < 1);
+
+	    Assert.IsFalse(_unset > 1);
+	    Assert.IsFalse(_default > 1);
+
+
+	    Assert.AreEqual(0, _unset.GetHashCode());
+	    Assert.AreEqual(0, _default.GetHashCode());
+	    Assert.AreEqual(1.GetHashCode(), t1.GetHashCode());
+
+	    Assert.AreEqual("UNSET", _unset.ToString());
+	    Assert.AreEqual("DEFAULT", _default.ToString());
+	    Assert.AreEqual("1", t1.ToString());
+
+	    i = (Int32)t2;
+	    Assert.AreEqual(2, i);
+
+
+	    Assert.AreEqual(1, t1.GetValueOrDefault());
+	    Assert.AreEqual(1, t1.GetValueOrDefault(2));
+	    Assert.AreEqual(0, _unset.GetValueOrDefault());
+	    Assert.AreEqual(2, _unset.GetValueOrDefault(2));
+
+
+	    Assert.IsTrue(_default == _default);
+	    Assert.IsTrue(_unset == _unset);
+	    Assert.IsTrue(t1 == t1);
+
+	    Object o = t1;
+	    Assert.IsTrue(t1.Equals((DataType<Int32>)o));
+	}
+
+
+
+
+
+    }
+}
---
+++ src/ValueTypes/Generic/DataType.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Spring2.Core.Types.Generic {
+    
+    [Serializable]
+    public struct DataType<T> : IDataType where T : struct, IComparable { //, IComparable, IFormattable, ISerializable {
+
+	private T myValue;
+	private TypeState myState;
+
+	public DataType(TypeState state) {
+	    myValue = default(T);
+	    myState = state;
+	}
+
+	public DataType(T value) {
+	    myValue = value;
+	    myState = TypeState.VALID;
+	}
+
+	public bool IsValid {
+	    get { return myState == TypeState.VALID; }
+	}
+
+	public bool IsDefault {
+	    get { return myState == TypeState.DEFAULT; }
+	}
+
+	public bool IsUnset {
+	    get { return myState == TypeState.UNSET; }
+	}
+
+
+	    public T Value {
+		get {
+		    if (myState != TypeState.VALID) {
+			throw new InvalidOperationException("DataType object must have a value.");
+		    }
+
+		    return myValue;
+		}
+	    }
+
+	// not right -- changed from Nullable
+	    public override bool Equals(object other) {
+		if (other == null)
+		    return false;
+
+		if (!(other is DataType<T>))
+		    return false;
+
+		return Equals((DataType<T>)other);
+	    }
+
+	    // not right -- changed from Nullable
+	    bool Equals(DataType<T> other) {
+		if (other == null) {
+		    return false;
+		}
+		if (other.myState != myState)
+		    return false;
+
+		if (IsValid == false)
+		    return true;
+
+		return other.myValue.Equals(myValue);
+	    }
+
+	    // not right -- changed from Nullable
+	    public override int GetHashCode() {
+		if (!IsValid)
+		    return 0;
+
+		return myValue.GetHashCode();
+	    }
+
+
+	    public T GetValueOrDefault() {
+		return GetValueOrDefault(default(T));
+	    }
+
+	    public T GetValueOrDefault(T def_value) {
+		if (!IsValid)
+		    return def_value;
+		else
+		    return myValue;
+	    }
+
+	    public override string ToString() {
+		if (IsValid) {
+		    return myValue.ToString();
+		} else if (IsDefault) {
+		    return "DEFAULT";
+		} else {
+		    return "UNSET";
+		}
+	    }
+
+	    public static implicit operator DataType<T>(T value) {
+		return new DataType<T>(value);
+	    }
+
+	    public static explicit operator T(DataType<T> value) {
+		return value.Value;
+	    }
+
+
+	    public static int Compare(DataType<T> leftHand, DataType<T> rightHand) {
+		if (leftHand.IsValid && rightHand.IsValid) {
+		    return leftHand.Value.CompareTo(rightHand.Value);
+		}
+
+		if (leftHand.myState == TypeState.UNSET) {
+		    if (rightHand.myState == TypeState.DEFAULT || rightHand.myState == TypeState.VALID) {
+			return -1;
+		    }
+
+		    if (rightHand.myState == TypeState.UNSET) {
+			return 0;
+		    }
+		}
+
+		if (leftHand.myState == TypeState.DEFAULT) {
+		    if (rightHand.myState == TypeState.DEFAULT) {
+			return 0;
+		    }
+
+		    if (rightHand.myState == TypeState.UNSET) {
+			return 1;
+		    }
+
+		    return -1;
+		}
+
+		if (leftHand.myState == TypeState.VALID) {
+		    return 1;
+		}
+
+		//should this throw an exception?
+		return 0;
+	    }
+
+	    public static int Compare(DataType<T> leftHand, T rightHand) {
+		if (leftHand.IsValid) {
+		    return leftHand.Value.CompareTo(rightHand);
+		}
+
+		if (leftHand.myState == TypeState.UNSET) {
+		    return -1;
+		}
+
+		if (leftHand.myState == TypeState.DEFAULT) {
+		    return -1;
+		}
+
+		//should this throw an exception?
+		return 0;
+	    }
+
+
+
+	    #region Equality operators
+	    public static bool operator ==(DataType<T> leftHand, DataType<T> rightHand) {
+		return Compare(leftHand, rightHand) == 0;
+	    }
+
+	    public static bool operator !=(DataType<T> leftHand, DataType<T> rightHand) {
+		return Compare(leftHand, rightHand) != 0;
+	    }
+
+	    public static bool operator ==(DataType<T> leftHand, T rightHand) {
+		return Compare(leftHand, rightHand) == 0;
+	    }
+
+	    public static bool operator !=(DataType<T> leftHand, T rightHand) {
+		return Compare(leftHand, rightHand) != 0;
+	    }
+	    #endregion    
+
+
+	    #region < and > operators
+	    public static bool operator <(DataType<T> leftHand, DataType<T> rightHand) {
+		return Compare(leftHand, rightHand) < 0;
+	    }
+
+	    public static bool operator >(DataType<T> leftHand, DataType<T> rightHand) {
+		return Compare(leftHand, rightHand) > 0;
+	    }
+
+	    public static bool operator <(DataType<T> leftHand, T rightHand) {
+		return Compare(leftHand, rightHand) < 0;
+	    }
+
+	    public static bool operator >(DataType<T> leftHand, T rightHand) {
+		return Compare(leftHand, rightHand) > 0;
+	    }
+	    #endregion    
+
+
+
+
+
+	    // These are called by the JIT
+	    // Ironicly, the C#  code is the same for these two,
+	    // however, on the inside they do somewhat different things
+	    static object Box(T? o) {
+		if (o == null)
+		    return null;
+		return (T)o;
+	    }
+
+	    static T? Unbox(object o) {
+		if (o == null)
+		    return null;
+		return (T)o;
+	    }
+
+
+    }
+
+}
