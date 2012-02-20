920ecf4a997f6fa6394e8804b4a6cd5b6fab2ca6
cort <none@none>
2012-02-20 02:53:51 +0000

    add class for String extension methods

---
+++ src/Util/ExtensionAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace System.Runtime.CompilerServices {
+    [AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
+    public class ExtensionAttribute : Attribute {
+    }
+}
---
+++ src/Util/StringExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Spring2.Core.Util {
+    public static class StringExtensions {
+
+	/// <summary>
+	/// Returns the first few characters of the string with a length
+	/// specified by the given parameter. If the string's length is less than the 
+	/// given length the complete string is returned. If length is zero or 
+	/// less an empty string is returned
+	/// </summary>
+	/// <param name="s">the string to process</param>
+	/// <param name="length">Number of characters to return</param>
+	/// <returns></returns>
+	public static string Left(this string s, int length) {
+	    length = Math.Max(length, 0);
+
+	    if (s.Length > length) {
+		return s.Substring(0, length);
+	    } else {
+		return s;
+	    }
+	}
+
+	/// <summary>
+	/// Returns the last few characters of the string with a length
+	/// specified by the given parameter. If the string's length is less than the 
+	/// given length the complete string is returned. If length is zero or 
+	/// less an empty string is returned
+	/// </summary>
+	/// <param name="s">the string to process</param>
+	/// <param name="length">Number of characters to return</param>
+	/// <returns></returns>
+	public static string Right(this string s, int length) {
+	    length = Math.Max(length, 0);
+
+	    if (s.Length > length) {
+		return s.Substring(s.Length - length, length);
+	    } else {
+		return s;
+	    }
+	}
+    }
+}
