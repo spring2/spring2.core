a679e93185962f39555b984c581b9a07fd888386
RuiFang <none@none>
2012-03-16 20:29:54 +0000

    added constructor for TestCancelableTaxProvider

--- src/Tax/Test/TestCancelableTaxProvider.cs
+++ src/Tax/Test/TestCancelableTaxProvider.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Spring2.Core.Types;
 
 namespace Spring2.Core.Tax.Test {
     public class TestCancelableTaxProvider : ICancelableTaxProvider {
+        public TestCancelableTaxProvider(StringType profileKey) { 
+        
+        }
+
         public void Cancel(TaxOrder order) {
             
         }
