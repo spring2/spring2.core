0eeaa760f29da4d7c9c176c97fa561ce598ccbf3
bryantc <none@none>
2014-02-13 18:05:23 +0000

    -Added support for EndiciaAddressValidationProvider

---
+++ src/AddressValidation/Endicia/DialAZipResponse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Spring2.Core.AddressValidation.Endicia {
+    [Serializable]
+    [XmlRoot(ElementName = "Dial-A-ZIP_Response")]
+    public class DialAZipResponse {
+	public string User { get; set; }
+	public string Command { get; set; }
+	public string ReturnCode { get; set; }
+	public string ZIP5 { get; set; }
+	public string Plus4 { get; set; }
+	public string Crt { get; set; }
+	public string AddrLine1 { get; set; }
+	public string AddrLine2 { get; set; }
+	public string AddrLine3 { get; set; }
+	public string AddrLineLast { get; set; }
+	public string City { get; set; }
+	public string State { get; set; }
+	public string DPDigits { get; set; }
+	public string CountryNo { get; set; }
+	public string LOTSeq { get; set; }
+	public string LOTAscend { get; set; }
+	public string CongressDist { get; set; }
+	public string AddrExists { get; set; }
+	public string AMSDate { get; set; }
+	public string AMSVersion { get; set; }
+	public string RecType { get; set; }
+	public string LACSStatus { get; set; }
+	public string LACSIndicator { get; set; }
+	public string LACSReturnCode { get; set; }
+	public string HSA { get; set; }
+	public string HSC { get; set; }
+	public string DPVFootNote { get; set; }
+	public string PRUrban { get; set; }
+	public string USPSFirmName { get; set; }
+	public string CountyName { get; set; }
+	public string RDI { get; set; }
+	public string UPSRural { get; set; }
+	public string ZIPLookupCount { get; set; }
+	public string AMSOpenRC { get; set; }
+	public string ComputerName { get; set; }
+	public string TimeStamp { get; set; }
+
+	// Hidden Element for Errors
+	public string Status { get; set; }
+    }
+}
---
+++ src/AddressValidation/Endicia/EndiciaAddressValidationProvider.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spring2.Core.AddressValidation;
+using Spring2.Core.Types;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+using System.Net;
+
+namespace Spring2.Core.AddressValidation.Endicia {
+    public class EndiciaAddressValidationProvider : IAddressValidationProvider {
+	protected string dialAZipUrlBaseFormat = "http://www.dial-a-zip.com/XML-Dial-A-ZIP/DAZService.asmx/MethodZIPValidate?input={0}";
+	protected const string COMMAND = "ZIP1";
+	protected string user = null;
+	protected string password = null;
+	protected string serialNumber = null;
+
+	protected string address1 = null;
+	protected string address2 = null;
+	protected string address3 = null;
+	protected string city = null;
+	protected string region = null;
+	protected string postalCode = null;
+	protected string country = null;
+
+	public AddressValidationResult Validate(StringType street, StringType city, StringType state, StringType postalCode, StringType countryCode) {
+	    this.address1 = street;
+	    this.city = city;
+	    this.region = state;
+	    this.postalCode = postalCode;
+	    this.country = countryCode;
+
+	    return Execute();
+	}
+
+	public AddressValidationResult Validate(StringType street1, StringType street2, StringType city, StringType state, StringType postalCode, StringType countryCode) {
+	    this.address1 = street1;
+	    this.address2 = street2;
+	    this.city = city;
+	    this.region = state;
+	    this.postalCode = postalCode;
+	    this.country = countryCode;
+
+	    return Execute();
+	}
+
+	public AddressValidationResult Validate(StringType street1, StringType street2, StringType street3, StringType city, StringType state, StringType postalCode, StringType countryCode) {
+	    this.address1 = street1;
+	    this.address2 = street2;
+	    this.address3 = street3;
+	    this.city = city;
+	    this.region = state;
+	    this.postalCode = postalCode;
+	    this.country = countryCode;
+
+	    return Execute();
+	}
+
+	public void SetCredentials(StringType user, StringType password, StringType serialNo) {
+	    this.user = user;
+	    this.password = password;
+	    this.serialNumber = serialNo;
+	}
+
+	private AddressValidationResult Execute() {
+	    if (string.IsNullOrEmpty(this.user) || string.IsNullOrEmpty(this.password) || string.IsNullOrEmpty(this.serialNumber)) {
+		throw new NotSupportedException("The credentials supplied are incomplete. Please verify and retry.");
+	    }
+	    DialAZipRequest request = new DialAZipRequest();
+	    request.USER = this.user;
+	    request.COMMAND = COMMAND;
+	    request.PASSWORD = this.password;
+	    request.SERIALNO = this.serialNumber;
+
+	    request.ADDRESS0 = "";
+	    request.ADDRESS1 = this.address1;
+	    request.ADDRESS2 = this.address2 ?? "";
+	    request.ADDRESS3 = string.Format("{0}, {1} {2}", this.city, this.region, this.postalCode);
+
+	    string xml = SerializeToString<DialAZipRequest>(request);
+	    HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(string.Format(dialAZipUrlBaseFormat, xml));
+	    webRequest.Method = "GET";
+
+	    HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
+	    string stringResponse = null;
+	    using (StreamReader reader = new StreamReader(webResponse.GetResponseStream())) {
+		stringResponse = reader.ReadToEnd();
+	    }
+
+	    DialAZipResponse response = DeserializeStringToObject<DialAZipResponse>(stringResponse);
+	    if (!string.IsNullOrEmpty(response.Status)) {
+		throw new NotSupportedException(response.Status);
+	    }
+
+	    AddressValidationResult result = new AddressValidationResult();
+	    result.ResponseXml = stringResponse;
+	    if (response.AddrExists == "TRUE") {
+		result.ResponseType = ResponseTypeEnum.VALID;
+	    } else {
+		result.ResponseType = ResponseTypeEnum.INVALID;
+	    }
+	    AddressList addressList = new AddressList();
+	    addressList.Add(new AddressData {
+		Street1 = response.AddrLine1,
+		Street2 = response.AddrLine2,
+		Street3 = response.AddrLine3,
+		City = response.City,
+		State = response.State,
+		PostalCode = string.Format("{0}{1}", response.ZIP5, (string.IsNullOrEmpty(response.Plus4) ? "" : string.Format("-{0}", response.Plus4)))
+	    });
+	    result.Addresses = addressList;
+	    return result;
+	}
+
+	private string SerializeToString<T>(T serializableObject) {
+	    XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+	    XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+	    XmlWriterSettings writerSettings = new XmlWriterSettings {
+		Encoding = Encoding.UTF8,
+		Indent = false,
+		OmitXmlDeclaration = true
+	    };
+
+	    using (StringWriter stringWriter = new StringWriter()) {
+		using (XmlWriter xmlWriter = XmlWriter.Create(stringWriter, writerSettings)) {
+		    serializer.Serialize(xmlWriter, serializableObject, namespaces);
+		    return stringWriter.ToString();
+		}
+	    }
+	}
+
+	private T DeserializeStringToObject<T>(string s) {
+	    XmlSerializer serializer = new XmlSerializer(typeof(T));
+	    using (StringReader reader = new StringReader(s)) {
+		return (T)serializer.Deserialize(reader);
+	    }
+	}
+    }
+}
